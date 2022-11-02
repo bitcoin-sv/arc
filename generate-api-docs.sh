cb6ba2148cc25e084da849030e2c4a2d5f9e9304
Siggi <github@icellan.net>
2022-11-02 12:45:35 +0100

    Updated API docs generation

--- generate-api-docs.sh
+++ generate-api-docs.sh
@@ -8,5 +8,10 @@ if ! [ -x "$(command -v shins)" ]; then
   npm install -g shins
 fi
 
+if ! [ -x "$(command -v swagger-cli)" ]; then
+  npm install -g swagger-cli
+fi
+
+swagger-cli bundle -o mapi.json mapi.yml
 widdershins --search false --language_tabs 'http:HTTP' 'javascript:JavaScript' 'java:Java' 'go:Go' 'ruby:Ruby' 'python:Python' 'shell:curl' --summary mapi.json -o mapi.md
 shins --inline --logo logo.png -o mapi.html mapi.md

b722bab0499f95b27255d9e9f4b963ecffa4e2e5
Simon Ordish <simon.ordish@masagi.co.uk>
2022-12-19 17:14:22 +0100

    Remove liquibase

--- schema/clearChecksumsLocal.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd $(dirname $BASH_SOURCE)
-
-./liquibase.sh --database=localhost:5432 --cmd=clearchecksums
--- schema/updateRemoteSQL.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd $(dirname $BASH_SOURCE)
-
-./liquibase.sh --database=localhost:15432 --cmd=updateSQL

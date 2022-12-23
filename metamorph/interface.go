47de33a543aa60beaa441d84cf7b01dd17b2cf43
Siggi <github@icellan.net>
2022-12-23 13:57:30 +0100

    Changed block tx to send mined transactions on channel, instead of writing directly to store

--- metamorph/interface.go
+++ metamorph/interface.go
@@ -6,5 +6,6 @@ type ProcessorI interface {
 	LoadUnseen()
 	ProcessTransaction(req *ProcessorRequest)
 	SendStatusForTransaction(hashStr string, status metamorph_api.Status, err error) (bool, error)
+	SendStatusMinedForTransaction(hash []byte) (bool, error)
 	GetStats() *ProcessorStats
 }

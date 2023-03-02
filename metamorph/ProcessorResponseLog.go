0cb0208ea72036e2640ac239b0c08ab30ff4d03a
Simon Ordish <simon.ordish@masagi.co.uk>
2023-03-02 17:20:57 +0100

    Working version of callback approach

--- metamorph/ProcessorResponseLog.go
+++
@@ -1,8 +0,0 @@
-package metamorph
-
-type ProcessorResponseLog struct {
-	DeltaT int64  `json:"delta_t"`
-	Status string `json:"status"`
-	Source string `json:"source,omitempty"`
-	Info   string `json:"info,omitempty"`
-}

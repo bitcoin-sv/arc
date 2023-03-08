441e0deac4b909ef6a816223c79ab7293d0837c3
Siggi <github@icellan.net>
2023-03-08 18:01:55 +0100

    Fixed memory leak in processor response deletion from map

--- metamorph/processor.go
+++ metamorph/processor.go
@@ -290,6 +290,7 @@ func (p *Processor) SendStatusMinedForTransaction(hash *chainhash.Hash, blockHas
 					p.mined.AddDuration(time.Since(resp.Start))
 				}
 
+				resp.Close()
 				p.processorResponseMap.Delete(hash)
 
 				if p.cbChannel != nil {
@@ -359,6 +360,7 @@ func (p *Processor) SendStatusForTransaction(hash *chainhash.Hash, status metamo
 
 				case metamorph_api.Status_MINED:
 					p.mined.AddDuration(time.Since(processorResponse.Start))
+					processorResponse.Close()
 					p.processorResponseMap.Delete(hash)
 
 				case metamorph_api.Status_REJECTED:
@@ -368,6 +370,7 @@ func (p *Processor) SendStatusForTransaction(hash *chainhash.Hash, status metamo
 					}
 
 					p.rejected.AddDuration(source, time.Since(processorResponse.Start))
+					// processorResponse.Close()
 					// p.processorResponseMap.Delete(hashStr)
 				}
 			},

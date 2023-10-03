b16ae04933afdad969d8c000126983082963d980
Nozim Mehrubonov <nozim.mehrubonov@gmail.com>
2023-10-03 13:06:11 +0100

    [BPAAS-1130] change random number generator function to more safe alternative (gosec warning).

--- blocktx/peer_handler.go
+++ blocktx/peer_handler.go
@@ -3,12 +3,13 @@ package blocktx
 import (
 	"bytes"
 	"context"
+	"crypto/rand"
 	"database/sql"
 	"encoding/binary"
 	"errors"
 	"fmt"
 	"io"
-	"math/rand"
+	"math/big"
 	"strings"
 	"time"
 
@@ -93,32 +94,38 @@ func init() {
 }
 
 func NewPeerHandler(logger utils.Logger, storeI store.Interface, blockCh chan *blocktx_api.Block) p2p.PeerHandlerI {
-	s := &PeerHandler{
-		store:    storeI,
-		blockCh:  blockCh,
-		logger:   logger,
-		workerCh: make(chan utils.Pair[*chainhash.Hash, p2p.PeerI], 100),
-		announcedCache: expiringmap.New[chainhash.Hash, []p2p.PeerI](10 * time.Minute).WithEvictionFunction(func(hash chainhash.Hash, peers []p2p.PeerI) bool {
-			msg := wire.NewMsgGetData()
+	evictionFunc := func(hash chainhash.Hash, peers []p2p.PeerI) bool {
+		msg := wire.NewMsgGetData()
 
-			if err := msg.AddInvVect(wire.NewInvVect(wire.InvTypeBlock, &hash)); err != nil {
-				logger.Errorf("EvictionFunc: could not create InvVect: %v", err)
-				return false
-			}
-
-			// Select a random peer to send the request to
-			peer := peers[rand.Intn(len(peers))]
-
-			if err := peer.WriteMsg(msg); err != nil {
-				logger.Errorf("EvictionFunc: failed to write message to peer: %v", err)
-				return false
-			}
-
-			logger.Infof("EvictionFunc: sent block request %s to peer %s", hash.String(), peer.String())
-
+		if err := msg.AddInvVect(wire.NewInvVect(wire.InvTypeBlock, &hash)); err != nil {
+			logger.Errorf("EvictionFunc: could not create InvVect: %v", err)
 			return false
-		}),
-		stats: safemap.New[string, *tracing.PeerHandlerStats](),
+		}
+		// Select a random peer to send the request to
+		n, err := rand.Int(rand.Reader, big.NewInt(int64(len(peers))))
+		if err != nil {
+			logger.Errorf("EvictionFunc: failed to pick random node: %v", err)
+			return false
+		}
+		peer := peers[n.Int64()]
+
+		if err := peer.WriteMsg(msg); err != nil {
+			logger.Errorf("EvictionFunc: failed to write message to peer: %v", err)
+			return false
+		}
+
+		logger.Infof("EvictionFunc: sent block request %s to peer %s", hash.String(), peer.String())
+
+		return false
+	}
+
+	s := &PeerHandler{
+		store:          storeI,
+		blockCh:        blockCh,
+		logger:         logger,
+		workerCh:       make(chan utils.Pair[*chainhash.Hash, p2p.PeerI], 100),
+		announcedCache: expiringmap.New[chainhash.Hash, []p2p.PeerI](10 * time.Minute).WithEvictionFunction(evictionFunc),
+		stats:          safemap.New[string, *tracing.PeerHandlerStats](),
 	}
 
 	_ = tracing.NewPeerHandlerCollector("blocktx", s.stats)

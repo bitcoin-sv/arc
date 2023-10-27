2796b53cff1fa3e92cb0a1b3dbf65c892ae974ef
Michael Böckli <michael.boeckli@gmail.com>
2023-10-27 13:28:16 +0200

    BPAAS-1054: Add unit test

--- api/handler/helpers_test.go
+++ api/handler/helpers_test.go
@@ -1,8 +1,12 @@
 package handler
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/labstack/echo/v4"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/require"
 )
@@ -21,4 +25,26 @@ func TestGetDefaultPolicy(t *testing.T) {
 
 		require.Equal(t, defaultPolicy, policy)
 	})
+}
+
+func TestCheckSwagger(t *testing.T) {
+	tt := []struct {
+		name string
+		path string
+	}{
+		{
+			name: "valid request",
+			path: "/v1/policy",
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			e := echo.New()
+			req := httptest.NewRequest(http.MethodPost, tc.path, strings.NewReader(""))
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+			CheckSwagger(e)
+		})
+	}
 }

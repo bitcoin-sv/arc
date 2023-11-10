1869feaef804df85109aad2f8b2fcdba780a94fd
Nozim Mehrubonov <nozim.mehrubonov@gmail.com>
2023-11-10 14:54:31 +0000

    Revert sql tests

--- blocktx/store/sql/sql_test.go
+++ blocktx/store/sql/sql_test.go
@@ -1,23 +1,19 @@
 package sql
 
 import (
+	"bytes"
 	"context"
 	"testing"
 
 	"github.com/bitcoin-sv/arc/blocktx/blocktx_api"
-	. "github.com/bitcoin-sv/arc/database_testing"
-	_ "github.com/lib/pq"
 	"github.com/libsv/go-p2p/chaincfg/chainhash"
+
+	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/stretchr/testify/suite"
 )
 
-type SQLTest struct {
-	DatabaseTestSuite
-}
-
-func (s *SQLTest) Test() {
+func TestInOut(t *testing.T) {
 	ctx := context.Background()
 
 	block := &blocktx_api.Block{
@@ -27,67 +23,75 @@ func (s *SQLTest) Test() {
 		Height:       1,
 	}
 
-	store, err := NewPostgresStore(DefaultParams)
-	require.NoError(s.T(), err)
+	s, err := New("sqlite_memory")
+	require.NoError(t, err)
 
-	blockId, err := store.InsertBlock(ctx, block)
-	require.NoError(s.T(), err)
+	blockId, err := s.InsertBlock(ctx, block)
+	require.NoError(t, err)
 
-	firstHash, err := chainhash.NewHash([]byte(GetRandomBytes()))
-	require.NoError(s.T(), err)
+	firstHash, err := chainhash.NewHashFromStr("b042f298deabcebbf15355aa3a13c7d7cfe96c44ac4f492735f936f8e50d06f6")
+	require.NoError(t, err)
 
 	transactions := []*blocktx_api.TransactionAndSource{
 		{Hash: firstHash[:], Source: "TEST"},
-		{Hash: []byte(GetRandomBytes()), Source: "TEST"},
-		{Hash: []byte(GetRandomBytes()), Source: "TEST"},
-		{Hash: []byte(GetRandomBytes()), Source: "TEST"},
-		{Hash: []byte(GetRandomBytes()), Source: "TEST"},
+		{Hash: []byte("test transaction hash 2"), Source: "TEST"},
+		{Hash: []byte("test transaction hash 3"), Source: "TEST"},
+		{Hash: []byte("test transaction hash 4"), Source: "TEST"},
+		{Hash: []byte("test transaction hash 5"), Source: "TEST"},
 	}
 
 	for _, txn := range transactions {
-		_, _, _, _, err = store.RegisterTransaction(ctx, txn)
-		require.NoError(s.T(), err)
+		_, _, _, _, err = s.RegisterTransaction(ctx, txn)
+		require.NoError(t, err)
 	}
 
-	_, _, _, _, err = store.RegisterTransaction(ctx, &blocktx_api.TransactionAndSource{
+	_, _, _, _, err = s.RegisterTransaction(ctx, &blocktx_api.TransactionAndSource{
 		Hash:   firstHash[:],
 		Source: "TEST",
 	})
-	require.NoError(s.T(), err)
+	require.NoError(t, err)
 
-	source, err := store.GetTransactionSource(ctx, firstHash)
-	require.NoError(s.T(), err)
-	assert.Equal(s.T(), "TEST", source)
+	source, err := s.GetTransactionSource(ctx, firstHash)
+	require.NoError(t, err)
+	assert.Equal(t, "TEST", source)
 
-	err = store.InsertBlockTransactions(ctx, blockId, transactions, make([]string, len(transactions)))
-	require.NoError(s.T(), err)
+	err = s.InsertBlockTransactions(ctx, blockId, transactions, make([]string, len(transactions)))
+	require.NoError(t, err)
 
-	txns, err := store.GetBlockTransactions(ctx, block)
-	require.NoError(s.T(), err)
+	txns, err := s.GetBlockTransactions(ctx, block)
+	require.NoError(t, err)
 
 	for i, txn := range txns.Transactions {
-		assert.Equal(s.T(), transactions[i].Hash, txn.Hash)
+		assert.Equal(t, bytes.Equal(transactions[i].Hash, txn.Hash), true)
 	}
 
 	height := uint64(1)
 
-	err = store.SetOrphanHeight(ctx, height, false)
-	require.NoError(s.T(), err)
+	err = s.SetOrphanHeight(ctx, height, false)
+	require.NoError(t, err)
 
-	block2, err := store.GetBlockForHeight(ctx, height)
-	require.NoError(s.T(), err)
-	assert.Equal(s.T(), string(block.Hash), string(block2.Hash))
+	block2, err := s.GetBlockForHeight(ctx, height)
+	require.NoError(t, err)
+	assert.Equal(t, bytes.Equal(block.Hash, block2.Hash), true)
 
-	err = store.OrphanHeight(ctx, height)
-	require.NoError(s.T(), err)
+	err = s.OrphanHeight(ctx, height)
+	require.NoError(t, err)
 
-	block3, err := store.GetBlockForHeight(ctx, height)
-	require.Error(s.T(), err)
-	assert.Nil(s.T(), block3)
+	block3, err := s.GetBlockForHeight(ctx, height)
+	require.Error(t, err)
+	assert.Nil(t, block3)
 
 }
 
-func TestSQLTest(t *testing.T) {
-	s := new(SQLTest)
-	suite.Run(t, s)
+func TestBlockNotExists(t *testing.T) {
+	ctx := context.Background()
+
+	s, err := New("sqlite_memory")
+	require.NoError(t, err)
+
+	height := uint64(1000000)
+
+	b, err := s.GetBlockForHeight(ctx, height)
+	require.Error(t, err)
+	assert.Nil(t, b)
 }

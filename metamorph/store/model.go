8996f57b55e19f92629e94732a99514e2c7f82b9
Nozim Mehrubonov <m8vn3m@gmail.com>
2023-12-17 20:14:45 +0000

    Fix compilation errors

---
+++ metamorph/store/model.go
@@ -0,0 +1,26 @@
+package store
+
+import "time"
+
+type Block struct {
+	Hash        string    `db:"hash"`
+	ProcessedAt time.Time `db:"processed_at"`
+	InsertedAt  time.Time `db:"inserted_at"`
+}
+
+type Transaction struct {
+	Hash          string    `db:"hash"`
+	StoredAt      time.Time `db:"stored_at"`
+	AnnouncedAt   time.Time `db:"announced_at"`
+	MinedAt       time.Time `db:"mined_at"`
+	Status        int       `db:"status"`
+	BlockHeight   int64     `db:"block_height"`
+	BlockHash     []byte    `db:"block_hash"`
+	CallbackURL   string    `db:"callback_url"`
+	CallbackToken string    `db:"callback_token"`
+	MerkleProof   string    `db:"merkle_proof"`
+	RejectReason  string    `db:"reject_reason"`
+	RawTX         []byte    `db:"raw_tx"`
+	LockedBy      string    `db:"locked_by"`
+	InsertedAt    time.Time `db:"inserted_at"`
+}

593dc78c4acaa2db77e104d99b0cc71ca922c10f
Michael Böckli <michael.boeckli@gmail.com>
2024-01-09 14:26:08 +0100

    Do not initialize transaction

--- metamorph/server_test.go
+++ metamorph/server_test.go
@@ -2,7 +2,6 @@ package metamorph_test
 
 import (
 	"context"
-	"encoding/hex"
 	"errors"
 	"fmt"
 	"log/slog"
@@ -21,7 +20,6 @@ import (
 	"github.com/bitcoin-sv/arc/testdata"
 	"github.com/libsv/go-bt/v2"
 	"github.com/libsv/go-p2p/chaincfg/chainhash"
-	"github.com/ordishs/go-bitcoin"
 	"github.com/ordishs/go-utils/stat"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -750,82 +748,6 @@ func TestStartGRPCServer(t *testing.T) {
 			time.Sleep(50 * time.Millisecond)
 
 			server.Shutdown()
-		})
-	}
-}
-
-func TestCheckUtxos(t *testing.T) {
-	validRawTx, err := hex.DecodeString("010000000000000000ef016b51c656fb06639ea6c1c3642a5ede9ecf9f749b95cb47d4e57eda7a3953b1c64c0000006a47304402201ade53acd924e90c0aeabbf9085d075acb23c4712e7f728a23979a466ab55e19022047a85963ce2eddc21573b4a6c0e7ccfec44153e74f9d03d31f955ff486449240412102f87ce69f6ba5444aed49c34470041189c1e1060acd99341959c0594002c61bf0ffffffffe8030000000000001976a914c2b6fd4319122b9b5156a2a0060d19864c24f49a88ac01e7030000000000001976a914c2b6fd4319122b9b5156a2a0060d19864c24f49a88ac00000000")
-	require.NoError(t, err)
-
-	tt := []struct {
-		name        string
-		txOut       *bitcoin.TXOut
-		getTxOutErr error
-		rawTx       []byte
-
-		expectedErrorStr string
-	}{
-		{
-			name:  "success",
-			txOut: &bitcoin.TXOut{},
-			rawTx: validRawTx,
-		},
-		{
-			name:  "invalid tx",
-			txOut: &bitcoin.TXOut{},
-			rawTx: []byte("invalid tx"),
-
-			expectedErrorStr: "failed to create bitcoin tx",
-		},
-		{
-			name:        "failed to get utxos",
-			txOut:       nil,
-			getTxOutErr: errors.New("failed to get utxos"),
-			rawTx:       validRawTx,
-
-			expectedErrorStr: "failed to get utxos",
-		},
-		{
-			name:  "utxos not found",
-			txOut: nil,
-			rawTx: validRawTx,
-
-			expectedErrorStr: "utxo c6b153397ada7ee5d447cb959b749fcf9ede5e2a64c3c1a69e6306fb56c6516b:76 not found",
-		},
-	}
-
-	for _, tc := range tt {
-		t.Run(tc.name, func(t *testing.T) {
-			metamorphStore := &MetamorphStoreMock{
-				GetFunc: func(ctx context.Context, key []byte) (*store.StoreData, error) {
-					return &store.StoreData{}, nil
-				},
-				RemoveCallbackerFunc: func(ctx context.Context, hash *chainhash.Hash) error {
-					return nil
-				},
-			}
-
-			btc := &ClientIMock{}
-
-			processor := &ProcessorIMock{
-				ShutdownFunc: func() {},
-			}
-
-			bitcoin := &BitcoinNodeMock{
-				GetTxOutFunc: func(txHex string, vout int, includeMempool bool) (*bitcoin.TXOut, error) {
-					return tc.txOut, tc.getTxOutErr
-				},
-			}
-			server := NewServer(metamorphStore, processor, btc, source, WithForceCheckUtxos(bitcoin))
-
-			_, err := server.CheckUtxos(context.Background(), 0, tc.rawTx)
-			if tc.expectedErrorStr != "" || err != nil {
-				require.ErrorContains(t, err, tc.expectedErrorStr)
-				return
-			} else {
-				require.NoError(t, err)
-			}
 		})
 	}
 }

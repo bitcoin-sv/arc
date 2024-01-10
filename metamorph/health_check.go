4b29384e7569a3f2ec531960af34416ba11ce033
Michael Böckli <michael.boeckli@gmail.com>
2024-01-10 09:34:36 +0100

    BAARC-98: unit tests

--- metamorph/health_check.go
+++ metamorph/health_check.go
@@ -2,15 +2,27 @@ package metamorph
 
 import (
 	"context"
-	"google.golang.org/grpc/health/grpc_health_v1"
 	"log/slog"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health/grpc_health_v1"
 )
+
+const (
+	readiness = "readiness"
+)
+
+//go:generate moq -pkg mocks -out ./mocks/health_watch_server_mock.go . HealthWatchServer
+type HealthWatchServer interface {
+	Send(*grpc_health_v1.HealthCheckResponse) error
+	grpc.ServerStream
+}
 
 func (s *Server) Check(ctx context.Context, req *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
 
 	s.logger.Info("checking health", slog.String("service", req.Service))
 
-	if req.Service == "readiness" {
+	if req.Service == readiness {
 		err := s.store.Ping(ctx)
 		if err != nil {
 			s.logger.Error("no connection to DB", slog.String("err", err.Error()))
@@ -35,7 +47,7 @@ func (s *Server) Check(ctx context.Context, req *grpc_health_v1.HealthCheckReque
 
 func (s *Server) Watch(req *grpc_health_v1.HealthCheckRequest, server grpc_health_v1.Health_WatchServer) error {
 	s.logger.Info("watching health", slog.String("service", req.Service))
-	if req.Service == "readiness" {
+	if req.Service == readiness {
 		err := s.store.Ping(context.Background())
 		if err != nil {
 			s.logger.Error("no connection to DB", slog.String("err", err.Error()))

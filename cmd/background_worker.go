afb7627378134804911365308aa80855293a0d75
Michael Böckli <michael.boeckli@gmail.com>
2024-01-17 10:55:10 +0100

    BAARC-106: Refactor background worker to schedule jobs for both blocktx and metamorph

--- cmd/background_worker.go
+++ cmd/background_worker.go
@@ -5,10 +5,12 @@ import (
 	"log/slog"
 	"time"
 
+	"github.com/bitcoin-sv/arc/api/transactionHandler"
 	"github.com/bitcoin-sv/arc/background_worker"
 	"github.com/bitcoin-sv/arc/background_worker/jobs"
 	"github.com/bitcoin-sv/arc/config"
 	"github.com/bitcoin-sv/arc/dbconn"
+	"github.com/bitcoin-sv/arc/metamorph/metamorph_api"
 	"github.com/go-co-op/gocron"
 )
 
@@ -17,6 +19,63 @@ const (
 )
 
 func StartBackGroundWorker(logger *slog.Logger) (func(), error) {
+	shutdownBlocktxScheduler, err := startBlocktxScheduler(logger)
+	if err != nil {
+		return nil, err
+	}
+
+	shutdownMetamorphScheduler, err := startMetamorphScheduler(logger)
+	if err != nil {
+		return nil, err
+	}
+
+	return func() {
+		shutdownBlocktxScheduler()
+		shutdownMetamorphScheduler()
+	}, err
+}
+
+func startMetamorphScheduler(logger *slog.Logger) (func(), error) {
+	logger.With("service", "background-worker")
+	metamorphAddress, err := config.GetString("metamorph.dialAddr")
+	if err != nil {
+		return nil, err
+	}
+	grpcMessageSize, err := config.GetInt("grpcMessageSize")
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := transactionHandler.DialGRPC(metamorphAddress, grpcMessageSize)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to metamorph server: %v", err)
+	}
+
+	meatmorphClearDataRetentionDays, err := config.GetInt("metamorph.db.cleanData.recordRetentionDays")
+	if err != nil {
+		return nil, err
+	}
+
+	executionIntervalHours, err := config.GetInt("metamorph.db.cleanData.executionIntervalHours")
+	if err != nil {
+		return nil, err
+	}
+
+	metamorphJobs := jobs.NewMetamorph(metamorph_api.NewMetaMorphAPIClient(conn), int32(meatmorphClearDataRetentionDays), logger)
+
+	scheduler := background_worker.NewMetamorphScheduler(gocron.NewScheduler(time.UTC), logger, executionIntervalHours)
+
+	scheduler.RunJob("clear metamorph transactions", metamorphJobs.ClearTransactions)
+
+	scheduler.Start()
+
+	return func() {
+		logger.Info("Shutting metamorph scheduler")
+		scheduler.Shutdown()
+	}, nil
+}
+
+func startBlocktxScheduler(logger *slog.Logger) (func(), error) {
 	dbHost, err := config.GetString("blocktx.db.postgres.host")
 	if err != nil {
 		return nil, err
@@ -70,24 +129,18 @@ func StartBackGroundWorker(logger *slog.Logger) (func(), error) {
 		RecordRetentionDays: cleanBlocksRecordRetentionDays,
 	}
 
-	jobs.Log(jobs.INFO, fmt.Sprintf("starting with %#v", params))
+	scheduler := background_worker.NewBlocktxScheduler(gocron.NewScheduler(time.UTC), intervalInHours, params, logger)
 
-	sched := background_worker.ARCScheduler{
-		Scheduler:       gocron.NewScheduler(time.UTC),
-		IntervalInHours: intervalInHours,
-		Params:          params,
-	}
+	clearJob := jobs.NewClearJob(logger)
 
-	clearJob := jobs.NewClearJob()
+	scheduler.RunJob("clear blocktx blocks", clearJob.ClearBlocks)
+	scheduler.RunJob("clear blocktx transactions", clearJob.ClearTransactions)
+	scheduler.RunJob("clear blocktx block transactions map", clearJob.ClearBlockTransactionsMap)
 
-	sched.RunJob("blocks", clearJob.ClearBlocks)
-	sched.RunJob("transactions", clearJob.ClearTransactions)
-	sched.RunJob("block transactions map", clearJob.ClearBlockTransactionsMap)
-
-	sched.Start()
+	scheduler.Start()
 
 	return func() {
-		logger.Info("Shutting Background-Worker")
-		sched.Shutdown()
+		logger.Info("Shutting down blocktx scheduler")
+		scheduler.Shutdown()
 	}, nil
 }

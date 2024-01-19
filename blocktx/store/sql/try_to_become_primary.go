2cca21a5795b1408514bd5a9fbb92ddab8d0ce68
Michael Böckli <michael.boeckli@gmail.com>
2024-01-19 08:59:19 +0100

    Remove unused functions from blocktx client

--- blocktx/store/sql/try_to_become_primary.go
+++ blocktx/store/sql/try_to_become_primary.go
@@ -9,7 +9,6 @@ import (
 
 const PrimaryDurationSecs = 2 * 60
 
-// GetBlockTransactions returns the transaction hashes for a given block hash.
 func (s *SQL) TryToBecomePrimary(ctx context.Context, myHostName string) error {
 	start := gocore.CurrentNanos()
 	defer func() {

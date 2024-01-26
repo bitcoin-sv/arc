1c5bbfc9816624cadd894b53d7a3747c12ef0f8c
ssilagadze <ssilagadze@purestorage.com>
2024-01-26 09:44:37 +0100

    BAARC-78: redefine err

--- api/transaction_handler/metamorph.go
+++ api/transaction_handler/metamorph.go
@@ -2,11 +2,12 @@ package transaction_handler
 
 import (
 	"context"
+	"errors"
 	"fmt"
-	"strings"
 	"time"
 
 	arc "github.com/bitcoin-sv/arc/api"
+	"github.com/bitcoin-sv/arc/metamorph"
 	"github.com/bitcoin-sv/arc/metamorph/metamorph_api"
 	"github.com/bitcoin-sv/arc/tracing"
 	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
@@ -71,7 +72,7 @@ func (m *Metamorph) GetTransactionStatus(ctx context.Context, txID string) (stat
 	tx, err = m.Client.GetTransactionStatus(ctx, &metamorph_api.TransactionStatusRequest{
 		Txid: txID,
 	})
-	if err != nil && !strings.Contains(err.Error(), "key could not be found") {
+	if err != nil && !errors.Is(err, metamorph.ErrNotFound) {
 		return nil, err
 	}
 
--- metamorph/server.go
+++ metamorph/server.go
@@ -12,7 +12,6 @@ import (
 	"sync"
 	"time"
 
-	"github.com/bitcoin-sv/arc/api/handler"
 	"github.com/bitcoin-sv/arc/blocktx"
 	"github.com/bitcoin-sv/arc/blocktx/blocktx_api"
 	"github.com/bitcoin-sv/arc/metamorph/metamorph_api"
@@ -37,10 +36,17 @@ func init() {
 	gocore.NewStat("PutTransaction", true)
 }
 
+// PtrTo returns a pointer to the given value.
+func PtrTo[T any](v T) *T {
+	return &v
+}
+
 const (
 	responseTimeout = 5 * time.Second
 	blocktxTimeout  = 1 * time.Second
 )
+
+var ErrNotFound = errors.New("key could not be found")
 
 type BitcoinNode interface {
 	GetTxOut(txHex string, vout int, includeMempool bool) (res *bitcoin.TXOut, err error)
@@ -205,7 +211,7 @@ func (s *Server) PutTransaction(ctx context.Context, req *metamorph_api.Transact
 		s.logger.Error("failed to validate callback URL", slog.String("err", err.Error()))
 		return nil, err
 	}
-	hash := handler.PtrTo(chainhash.DoubleHashH(req.GetRawTx()))
+	hash := PtrTo(chainhash.DoubleHashH(req.GetRawTx()))
 	status := metamorph_api.Status_RECEIVED
 
 	// Convert gRPC req to store.StoreData struct...
@@ -258,7 +264,7 @@ func (s *Server) PutTransactions(ctx context.Context, req *metamorph_api.Transac
 		}
 
 		status := metamorph_api.Status_RECEIVED
-		hash := handler.PtrTo(chainhash.DoubleHashH(txReq.GetRawTx()))
+		hash := PtrTo(chainhash.DoubleHashH(txReq.GetRawTx()))
 		timeout = txReq.GetMaxTimeout()
 
 		// Convert gRPC req to store.StoreData struct...

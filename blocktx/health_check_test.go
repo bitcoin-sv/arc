e76a80895d6379eec17111f3429f635dff9ef3f2
ssilagadze <ssilagadze@purestorage.com>
2024-02-20 09:15:39 +0100

    add mock

--- blocktx/health_check_test.go
+++ blocktx/health_check_test.go
@@ -1,4 +1,4 @@
-package blocktx
+package blocktx_test
 
 import (
 	"context"
@@ -6,13 +6,10 @@ import (
 	"log/slog"
 	"os"
 	"testing"
-	"time"
 
-	"github.com/bitcoin-sv/arc/blocktx/blocktx_api"
+	"github.com/bitcoin-sv/arc/blocktx"
+	"github.com/bitcoin-sv/arc/blocktx/mocks"
 	"github.com/bitcoin-sv/arc/blocktx/store"
-	"github.com/bitcoin-sv/arc/metamorph/mocks"
-	"github.com/bitcoin-sv/arc/testdata"
-	"github.com/libsv/go-p2p/chaincfg/chainhash"
 	"github.com/libsv/go-p2p/wire"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/health/grpc_health_v1"
@@ -24,37 +21,28 @@ func TestCheck(t *testing.T) {
 		service            string
 		pingErr            error
 		processorHealthErr error
-		getBlockErr        error
+		primaryErr         error
 
 		expectedStatus grpc_health_v1.HealthCheckResponse_ServingStatus
 	}{
 		{
-			name:        "liveness - healthy",
-			service:     "liveness",
-			getBlockErr: errors.New("failed to get block"),
-
-			expectedStatus: grpc_health_v1.HealthCheckResponse_SERVING,
-		},
-		{
-			name:        "readiness - healthy",
-			service:     "readiness",
-			getBlockErr: errors.New("failed to get block"),
-
-			expectedStatus: grpc_health_v1.HealthCheckResponse_SERVING,
-		},
-		{
-			name:        "readiness - ping error",
-			service:     "readiness",
-			pingErr:     errors.New("no connection"),
-			getBlockErr: errors.New("failed to get block"),
+			name:       "liveness - peer not found",
+			service:    "readiness",
+			primaryErr: nil,
 
 			expectedStatus: grpc_health_v1.HealthCheckResponse_NOT_SERVING,
 		},
 		{
-			name:               "readiness - unhealthy processor",
-			service:            "readiness",
-			processorHealthErr: errors.New("unhealthy processor"),
-			getBlockErr:        errors.New("failed to get block"),
+			name:       "db error - not connected",
+			service:    "readiness",
+			primaryErr: errors.New("not connected"),
+
+			expectedStatus: grpc_health_v1.HealthCheckResponse_NOT_SERVING,
+		},
+		{
+			name:       "db error - not connected",
+			service:    "readiness",
+			primaryErr: nil,
 
 			expectedStatus: grpc_health_v1.HealthCheckResponse_NOT_SERVING,
 		},
@@ -62,23 +50,15 @@ func TestCheck(t *testing.T) {
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
+			const batchSize = 4
 
-			storeMock := &store.InterfaceMock{
-				GetBlockFunc: func(ctx context.Context, hash *chainhash.Hash) (*blocktx_api.Block, error) {
-					return &blocktx_api.Block{}, tc.getBlockErr
+			var storeMock = &store.InterfaceMock{
+				GetBlockGapsFunc: func(ctx context.Context, heightRange int) ([]*store.BlockGap, error) {
+					return nil, nil
 				},
-				InsertBlockFunc: func(ctx context.Context, block *blocktx_api.Block) (uint64, error) {
-					return 0, nil
-				},
-				MarkBlockAsDoneFunc: func(ctx context.Context, hash *chainhash.Hash, size uint64, txCount uint64) error {
-					return nil
-				},
+
 				GetPrimaryFunc: func(ctx context.Context) (string, error) {
-					hostName, err := os.Hostname()
-					return hostName, err
-				},
-				TryToBecomePrimaryFunc: func(ctx context.Context, myHostName string) error {
-					return nil
+					return "", tc.primaryErr
 				},
 			}
 
@@ -86,33 +66,17 @@ func TestCheck(t *testing.T) {
 				Service: tc.service,
 			}
 
-			// build peer manager
 			logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+			peerHandler, err := blocktx.NewPeerHandler(logger, storeMock, 100, []string{}, wire.TestNet, blocktx.WithTransactionBatchSize(batchSize))
+			require.NoError(t, err)
 
-			txChan := make(chan []byte, 10)
-
-			txChan <- testdata.TX1Hash[:]
-			txChan <- testdata.TX2Hash[:]
-			txChan <- testdata.TX3Hash[:]
-			txChan <- testdata.TX4Hash[:]
-
-			peerHandler, _ := NewPeerHandler(
-				logger,
-				storeMock,
-				100,
-				[]string{},
-				wire.TestNet,
-				WithRegisterTxsInterval(time.Millisecond*20),
-				WithTxChan(txChan),
-				WithRegisterTxsBatchSize(3),
-			)
-
-			server := NewServer(storeMock, logger, peerHandler)
-
+			server := blocktx.NewServer(storeMock, logger, peerHandler)
 			resp, err := server.Check(context.Background(), req)
 			require.NoError(t, err)
 
 			require.Equal(t, tc.expectedStatus, resp.Status)
+
+			peerHandler.Shutdown()
 		})
 	}
 }
@@ -124,6 +88,7 @@ func TestWatch(t *testing.T) {
 		pingErr            error
 		processorHealthErr error
 		getBlockErr        error
+		primaryErr         error
 
 		expectedStatus grpc_health_v1.HealthCheckResponse_ServingStatus
 	}{
@@ -132,55 +97,24 @@ func TestWatch(t *testing.T) {
 			service:            "liveness",
 			processorHealthErr: errors.New("unhealthy processor"),
 			getBlockErr:        errors.New("failed to get block"),
+			primaryErr:         nil,
 
 			expectedStatus: grpc_health_v1.HealthCheckResponse_SERVING,
-		},
-		{
-			name:               "readiness - healty",
-			service:            "readiness",
-			processorHealthErr: errors.New("unhealthy processor"),
-			getBlockErr:        errors.New("failed to get block"),
-
-			expectedStatus: grpc_health_v1.HealthCheckResponse_SERVING,
-		},
-		{
-			name:               "readiness - ping error",
-			service:            "readiness",
-			pingErr:            errors.New("no connection"),
-			processorHealthErr: errors.New("unhealthy processor"),
-			getBlockErr:        errors.New("failed to get block"),
-
-			expectedStatus: grpc_health_v1.HealthCheckResponse_NOT_SERVING,
-		},
-		{
-			name:               "readiness - unhealthy processor",
-			service:            "readiness",
-			processorHealthErr: errors.New("unhealthy processor"),
-			getBlockErr:        errors.New("failed to get block"),
-
-			expectedStatus: grpc_health_v1.HealthCheckResponse_NOT_SERVING,
 		},
 	}
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
 
-			storeMock := &store.InterfaceMock{
-				GetBlockFunc: func(ctx context.Context, hash *chainhash.Hash) (*blocktx_api.Block, error) {
-					return &blocktx_api.Block{}, tc.getBlockErr
+			const batchSize = 4
+
+			var storeMock = &store.InterfaceMock{
+				GetBlockGapsFunc: func(ctx context.Context, heightRange int) ([]*store.BlockGap, error) {
+					return nil, nil
 				},
-				InsertBlockFunc: func(ctx context.Context, block *blocktx_api.Block) (uint64, error) {
-					return 0, nil
-				},
-				MarkBlockAsDoneFunc: func(ctx context.Context, hash *chainhash.Hash, size uint64, txCount uint64) error {
-					return nil
-				},
+
 				GetPrimaryFunc: func(ctx context.Context) (string, error) {
-					hostName, err := os.Hostname()
-					return hostName, err
-				},
-				TryToBecomePrimaryFunc: func(ctx context.Context, myHostName string) error {
-					return nil
+					return "", tc.primaryErr
 				},
 			}
 
@@ -188,28 +122,11 @@ func TestWatch(t *testing.T) {
 				Service: tc.service,
 			}
 
-			// build peer manager
 			logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+			peerHandler, err := blocktx.NewPeerHandler(logger, storeMock, 100, []string{}, wire.TestNet, blocktx.WithTransactionBatchSize(batchSize))
+			require.NoError(t, err)
 
-			txChan := make(chan []byte, 10)
-
-			txChan <- testdata.TX1Hash[:]
-			txChan <- testdata.TX2Hash[:]
-			txChan <- testdata.TX3Hash[:]
-			txChan <- testdata.TX4Hash[:]
-
-			peerHandler, _ := NewPeerHandler(
-				logger,
-				storeMock,
-				100,
-				[]string{},
-				wire.TestNet,
-				WithRegisterTxsInterval(time.Millisecond*20),
-				WithTxChan(txChan),
-				WithRegisterTxsBatchSize(3),
-			)
-
-			server := NewServer(storeMock, logger, peerHandler)
+			server := blocktx.NewServer(storeMock, logger, peerHandler)
 
 			watchServer := &mocks.HealthWatchServerMock{
 				SendFunc: func(healthCheckResponse *grpc_health_v1.HealthCheckResponse) error {
@@ -218,7 +135,7 @@ func TestWatch(t *testing.T) {
 				},
 			}
 
-			err := server.Watch(req, watchServer)
+			err = server.Watch(req, watchServer)
 			require.NoError(t, err)
 		})
 	}

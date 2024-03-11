26a715c346793c3946f76ba6d06d6888604782c2
ssilagadze <ssilagadze@purestorage.com>
2024-03-11 15:37:35 +0100

    separating resources into corresponding folders

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -10,5 +10,5 @@ fi
 
 swagger-cli bundle -o api/arc.json api/arc.yml
 cp api/arc.json doc/
-cp logo.png doc/dist/
+cp web/logo.png doc/dist/
 widdershins --search false --language_tabs 'http:HTTP' 'javascript:JavaScript' 'java:Java' 'go:Go' 'ruby:Ruby' 'python:Python' 'shell:curl' --summary api/arc.json -o doc/api.md

26676b459e623a17d82e49d03226700682d63efd
ssilagadze <ssilagadze@purestorage.com>
2024-03-15 09:19:22 +0100

    fix test

--- internal/metamorph/store/postgresql/postgres_test.go
+++ internal/metamorph/store/postgresql/postgres_test.go
@@ -363,7 +363,7 @@ func TestPostgresDB(t *testing.T) {
 		err = postgresDB.Set(ctx, testdata.TX6Hash[:], tx6Data)
 		require.NoError(t, err)
 
-		// will not be updated because it is locked by metamorph-3
+		// will now be updated even though it is locked by metamorph-3
 		metamorph3Hash, err := chainhash.NewHashFromStr("538808e847d0add40ed9622fff53954c79e1f52db7c47ea0b6cdc0df972f3dcd")
 		require.NoError(t, err)
 
@@ -410,17 +410,14 @@ func TestPostgresDB(t *testing.T) {
 
 		statusUpdates, err := postgresDB.UpdateStatusBulk(ctx, updates)
 		require.NoError(t, err)
-		fmt.Println(" aq var ", statusUpdates[0].Hash, statusUpdates[0].RawTx, statusUpdates[0].Status)
-		fmt.Println(" aq var ", statusUpdates[1].Hash, statusUpdates[1].RawTx, statusUpdates[1].Status)
-		fmt.Println(" aq var ", statusUpdates[2].Hash, statusUpdates[2].RawTx, statusUpdates[2].Status)
-		require.Len(t, statusUpdates, 2)
+		require.Len(t, statusUpdates, 3)
 
-		assert.Equal(t, metamorph_api.Status_REQUESTED_BY_NETWORK, statusUpdates[0].Status)
-		assert.Equal(t, testdata.TX1RawBytes, statusUpdates[0].RawTx)
+		assert.Equal(t, metamorph_api.Status_REQUESTED_BY_NETWORK, statusUpdates[1].Status)
+		assert.Equal(t, testdata.TX1RawBytes, statusUpdates[1].RawTx)
 
-		assert.Equal(t, metamorph_api.Status_REJECTED, statusUpdates[1].Status)
-		assert.Equal(t, "missing inputs", statusUpdates[1].RejectReason)
-		assert.Equal(t, testdata.TX6RawBytes, statusUpdates[1].RawTx)
+		assert.Equal(t, metamorph_api.Status_REJECTED, statusUpdates[2].Status)
+		assert.Equal(t, "missing inputs", statusUpdates[2].RejectReason)
+		assert.Equal(t, testdata.TX6RawBytes, statusUpdates[2].RawTx)
 
 		returnedDataRejected, err := postgresDB.Get(ctx, testdata.TX1Hash[:])
 		require.NoError(t, err)

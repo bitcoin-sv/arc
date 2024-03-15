9e05c4fc29386c567d02bb3068a7813887b622a2
ssilagadze <ssilagadze@purestorage.com>
2024-03-15 11:57:07 +0100

    check transaction before calculating merkle path

---
+++ internal/blocktx/store/postgresql/transaction_exists.go
@@ -0,0 +1,29 @@
+package postgresql
+
+import (
+	"context"
+
+	"github.com/libsv/go-p2p/chaincfg/chainhash"
+	"github.com/ordishs/gocore"
+)
+
+func (p *PostgreSQL) TransactionExists(ctx context.Context, hash chainhash.Hash) (bool, error) {
+	start := gocore.CurrentNanos()
+	defer func() {
+		gocore.NewStat("blocktx").NewStat("GetBlockGaps").AddTime(start)
+	}()
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	query := "SELECT EXISTS(SELECT 1 FROM transactions WHERE hash = $1)"
+
+	// Execute the query
+	var exists bool
+	err := p.db.QueryRow(query, hash).Scan(&exists)
+	if err != nil {
+		return false, err
+	}
+
+	return exists, nil
+}

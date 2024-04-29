8f48edb49ad0360a76e235d00f68995563998cb1
Michael Böckli <michael.boeckli@gmail.com>
2024-04-29 09:31:05 +0200

    Make lint

--- cmd/arc/services/metamorph.go
+++ cmd/arc/services/metamorph.go
@@ -121,7 +121,10 @@ func StartMetamorph(logger *slog.Logger) (func(), error) {
 	metamorphProcessor.StartRequestingSeenOnNetworkTxs()
 	metamorphProcessor.StartProcessStatusUpdatesInStorage()
 	metamorphProcessor.StartProcessMinedCallbacks()
-	metamorphProcessor.StartCollectStats()
+	err = metamorphProcessor.StartCollectStats()
+	if err != nil {
+		return nil, fmt.Errorf("failed to start collect stats: %v", err)
+	}
 
 	go func() {
 		for message := range statusMessageCh {

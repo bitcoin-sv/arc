ac48301c43af30cf445136776906276c17749c7a
Michael Böckli <michael.boeckli@gmail.com>
2024-05-13 16:22:43 +0200

    BAARC-159: If get unmined function fails do not continue but break

--- internal/metamorph/processor.go
+++ internal/metamorph/processor.go
@@ -415,7 +415,7 @@ func (p *Processor) StartProcessExpiredTransactions() {
 					unminedTxs, err := p.store.GetUnmined(ctx, getUnminedSince, loadUnminedLimit, offset)
 					if err != nil {
 						p.logger.Error("Failed to get unmined transactions", slog.String("err", err.Error()))
-						continue
+						break
 					}
 
 					offset += loadUnminedLimit

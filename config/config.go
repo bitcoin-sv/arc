7a746f0d2636f3b8ea6794f003a8db0f4d3bcf2f
kuba-4chain <jakub.smolka@4chain.studio>
2024-06-13 16:48:09 +0200

    feat(ARCO-90): default config represented in code

--- config/config.go
+++ config/config.go
@@ -3,19 +3,33 @@ package config
 import (
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/mitchellh/mapstructure"
+	"github.com/ordishs/go-bitcoin"
 	"github.com/spf13/viper"
 )
 
 type ArcConfig struct {
-	// root config
-	PeerRpc *PeerRpcConfig `json:"peerRpc" mapstructure:"peerRpc"`
-	// peers
-	// metamorph
-	// blocktx
-	// api
-	// k8sWatcher
+	LogLevel           string            `json:"logLevel" mapstructure:"logLevel"`
+	LogFormat          string            `json:"logFormat" mapstructure:"logFormat"`
+	ProfilerAddr       string            `json:"profilerAddr" mapstructure:"profilerAddr"`
+	PrometheusEndpoint string            `json:"prometheusEndpoint" mapstructure:"prometheusEndpoint"`
+	PrometheusAddr     string            `json:"prometheusAddr" mapstructure:"prometheusAddr"`
+	GrpcMessageSize    int               `json:"grpcMessageSize" mapstructure:"grpcMessageSize"`
+	Network            string            `json:"network" mapstructure:"network"`
+	QueueURL           string            `json:"queueURL" mapstructure:"queueURL"`
+	Tracing            *TracingConfig    `json:"tracing" mapstructure:"tracing"`
+	PeerRpc            *PeerRpcConfig    `json:"peerRpc" mapstructure:"peerRpc"`
+	Peers              []*PeerConfig     `json:"peers" mapstructure:"peers"`
+	Metamorph          *MetamorphConfig  `json:"metamorph" mapstructure:"metamorph"`
+	Blocktx            *BlocktxConfig    `json:"blocktx" mapstructure:"blocktx"`
+	Api                *ApiConfig        `json:"api" mapstructure:"api"`
+	K8sWatcher         *K8sWatcherConfig `json:"k8sWatcher" mapstructure:"k8sWatcher"`
+}
+
+type TracingConfig struct {
+	DialAddr string `json:"dialAddr" mapstructure:"dialAddr"`
 }
 
 type PeerRpcConfig struct {
@@ -23,6 +37,86 @@ type PeerRpcConfig struct {
 	User     string `json:"user" mapstructure:"user"`
 	Host     string `json:"host" mapstructure:"host"`
 	Port     int    `json:"port" mapstructure:"port"`
+}
+
+type PeerConfig struct {
+	Host string          `json:"host" mapstructure:"host"`
+	Port *PeerPortConfig `json:"port" mapstructure:"port"`
+}
+
+type PeerPortConfig struct {
+	P2P int `json:"p2p" mapstructure:"p2p"`
+	ZMQ int `json:"zmq" mapstructure:"zmq"`
+}
+
+type MetamorphConfig struct {
+	ListenAddr                  string        `json:"listenAddr" mapstructure:"listenAddr"`
+	DialAddr                    string        `json:"dialAddr" mapstructure:"dialAddr"`
+	Db                          *DbConfig     `json:"db" mapstructure:"db"`
+	ProcessorCacheExpiryTime    time.Duration `json:"processorCacheExpiryTime" mapstructure:"processorCacheExpiryTime"`
+	MaxRetries                  int           `json:"maxRetries" mapstructure:"maxRetries"`
+	ProcessStatusUpdateInterval time.Duration `json:"processStatusUpdateInterval" mapstructure:"processStatusUpdateInterval"`
+	CheckSeenOnNetworkOlderThan time.Duration `json:"checkSeenOnNetworkOlderThan" mapstructure:"checkSeenOnNetworkOlderThan"`
+	CheckSeenOnNetworkPeriod    time.Duration `json:"checkSeenOnNetworkPeriod" mapstructure:"checkSeenOnNetworkPeriod"`
+	MonitorPeersInterval        time.Duration `json:"monitorPeersInterval" mapstructure:"monitorPeersInterval"`
+	CheckUtxos                  bool          `json:"checkUtxos" mapstructure:"checkUtxos"`
+	ProfilerAddr                string        `json:"profilerAddr" mapstructure:"profilerAddr"`
+	Health                      *HealthConfig `json:"health" mapstructure:"health"`
+	RejectCallbackContaining    []string      `json:"rejectCallbackContaining" mapstructure:"rejectCallbackContaining"`
+	Stats                       *StatsConfig  `json:"stats" mapstructure:"stats"`
+}
+
+type BlocktxConfig struct {
+	ListenAddr                    string              `json:"listenAddr" mapstructure:"listenAddr"`
+	DialAddr                      string              `json:"dialAddr" mapstructure:"dialAddr"`
+	HealthServerDialAddr          string              `json:"healthServerDialAddr" mapstructure:"healthServerDialAddr"`
+	Db                            *DbConfig           `json:"db" mapstructure:"db"`
+	RecordRetentionDays           int                 `json:"recordRetentionDays" mapstructure:"recordRetentionDays"`
+	ProfilerAddr                  string              `json:"profilerAddr" mapstructure:"profilerAddr"`
+	RegisterTxsInterval           time.Duration       `json:"registerTxsInterval" mapstructure:"registerTxsInterval"`
+	FillGapsInterval              time.Duration       `json:"fillGapsInterval" mapstructure:"fillGapsInterval"`
+	MaxAllowedBlockHeightMismatch int                 `json:"maxAllowedBlockHeightMismatch" mapstructure:"maxAllowedBlockHeightMismatch"`
+	MessageQueue                  *MessageQueueConfig `json:"mq" mapstructure:"mq"`
+}
+
+type DbConfig struct {
+	Mode     string          `json:"mode" mapstructure:"mode"`
+	Postgres *PostgresConfig `json:"postgres" mapstructure:"postgres"`
+}
+
+type PostgresConfig struct {
+	Host         string `json:"host" mapstructure:"host"`
+	Port         int    `json:"port" mapstructure:"port"`
+	Name         string `json:"name" mapstructure:"name"`
+	User         string `json:"user" mapstructure:"user"`
+	Password     string `json:"password" mapstructure:"password"`
+	MaxIdleConns int    `json:"maxIdleConns" mapstructure:"maxIdleConns"`
+	MaxOpenConns int    `json:"maxOpenConns" mapstructure:"maxOpenConns"`
+	SslMode      string `json:"sslMode" mapstructure:"sslMode"`
+}
+
+type HealthConfig struct {
+	SeverDialAddr             string `json:"serverDialAddr" mapstructure:"serverDialAddr"`
+	MinimumHealthyConnections int    `json:"minimumHealthyConnections" mapstructure:"minimumHealthyConnections"`
+}
+
+type StatsConfig struct {
+	NotSeenTimeLimit  time.Duration `json:"notSeenTimeLimit" mapstructure:"notSeenTimeLimit"`
+	NotMinedTimeLimit time.Duration `json:"notMinedTimeLimit" mapstructure:"notMinedTimeLimit"`
+}
+
+type MessageQueueConfig struct {
+	TxsMinedMaxBatchSize int `json:"txsMinedMaxBatchSize" mapstructure:"txsMinedMaxBatchSize"`
+}
+
+type ApiConfig struct {
+	Address       string            `json:"address" mapstructure:"address"`
+	WocApiKey     string            `json:"wocApiKey" mapstructure:"wocApiKey"`
+	DefaultPolicy *bitcoin.Settings `json:"defaultPolicy" mapstructure:"defaultPolicy"`
+}
+
+type K8sWatcherConfig struct {
+	Namespace string `json:"namespace" mapstructure:"namespace"`
 }
 
 func Load(configFile string) (*ArcConfig, error) {
--- config/defaults.go
+++ config/defaults.go
@@ -1,8 +1,34 @@
 package config
 
+import (
+	"time"
+
+	"github.com/ordishs/go-bitcoin"
+)
+
 func getDefaultArcConfig() *ArcConfig {
 	return &ArcConfig{
-		PeerRpc: getDefaultPeerRpcConfig(),
+		LogLevel:           "DEBUG",
+		LogFormat:          "text",
+		ProfilerAddr:       "localhost:9999",
+		PrometheusEndpoint: "/metrics",
+		PrometheusAddr:     ":2112",
+		GrpcMessageSize:    100000000,
+		Network:            "regtest",
+		QueueURL:           "nats://nats:4222",
+		Tracing:            getTracingConfig(),
+		PeerRpc:            getDefaultPeerRpcConfig(),
+		Peers:              getPeersConfig(),
+		Metamorph:          getMetamorphConfig(),
+		Blocktx:            getBlocktxConfig(),
+		Api:                getApiConfig(),
+		K8sWatcher:         getK8sWatcherConfig(),
+	}
+}
+
+func getTracingConfig() *TracingConfig {
+	return &TracingConfig{
+		DialAddr: "http://localhost:4317",
 	}
 }
 
@@ -12,5 +38,129 @@ func getDefaultPeerRpcConfig() *PeerRpcConfig {
 		User:     "bitcoin",
 		Host:     "localhost",
 		Port:     18332,
+	}
+}
+
+func getPeersConfig() []*PeerConfig {
+	return []*PeerConfig{
+		{
+			Host: "localhost",
+			Port: &PeerPortConfig{
+				P2P: 18333,
+				ZMQ: 28332,
+			},
+		},
+		{
+			Host: "localhost",
+			Port: &PeerPortConfig{
+				P2P: 18334,
+			},
+		},
+		{
+			Host: "localhost",
+			Port: &PeerPortConfig{
+				P2P: 18335,
+			},
+		},
+	}
+}
+
+func getMetamorphConfig() *MetamorphConfig {
+	return &MetamorphConfig{
+		ListenAddr:                  "localhost:8001",
+		DialAddr:                    "localhost:8001",
+		Db:                          getDbConfig("metamorph"),
+		ProcessorCacheExpiryTime:    24 * time.Hour,
+		CheckSeenOnNetworkOlderThan: 3 * time.Hour,
+		CheckSeenOnNetworkPeriod:    4 * time.Hour,
+		MonitorPeersInterval:        60 * time.Second,
+		CheckUtxos:                  false,
+		ProfilerAddr:                "localhost:9992",
+		Health: &HealthConfig{
+			SeverDialAddr:             "localhost:8005",
+			MinimumHealthyConnections: 2,
+		},
+		RejectCallbackContaining: []string{"http://localhost", "https://localhost"},
+		Stats: &StatsConfig{
+			NotSeenTimeLimit:  10 * time.Minute,
+			NotMinedTimeLimit: 20 * time.Minute,
+		},
+	}
+}
+
+func getBlocktxConfig() *BlocktxConfig {
+	return &BlocktxConfig{
+		ListenAddr:                    "localhost:8011",
+		DialAddr:                      "localhost:8011",
+		HealthServerDialAddr:          "localhost:8006",
+		Db:                            getDbConfig("blocktx"),
+		RecordRetentionDays:           28,
+		ProfilerAddr:                  "localhost:9993",
+		RegisterTxsInterval:           10 * time.Second,
+		FillGapsInterval:              15 * time.Minute,
+		MaxAllowedBlockHeightMismatch: 3,
+		MessageQueue: &MessageQueueConfig{
+			TxsMinedMaxBatchSize: 20,
+		},
+	}
+}
+
+func getApiConfig() *ApiConfig {
+	return &ApiConfig{
+		Address:   "localhost:9090",
+		WocApiKey: "mainnet_XXXXXXXXXXXXXXXXXXXX",
+		DefaultPolicy: &bitcoin.Settings{
+			ExcessiveBlockSize:              2000000000,
+			BlockMaxSize:                    512000000,
+			MaxTxSizePolicy:                 100000000,
+			MaxOrphanTxSize:                 1000000000,
+			DataCarrierSize:                 4294967295,
+			MaxScriptSizePolicy:             100000000,
+			MaxOpsPerScriptPolicy:           4294967295,
+			MaxScriptNumLengthPolicy:        10000,
+			MaxPubKeysPerMultisigPolicy:     4294967295,
+			MaxTxSigopsCountsPolicy:         4294967295,
+			MaxStackMemoryUsagePolicy:       100000000,
+			MaxStackMemoryUsageConsensus:    200000000,
+			LimitAncestorCount:              10000,
+			LimitCPFPGroupMembersCount:      25,
+			MaxMempool:                      2000000000,
+			MaxMempoolSizedisk:              0,
+			MempoolMaxPercentCPFP:           10,
+			AcceptNonStdOutputs:             true,
+			DataCarrier:                     true,
+			MinMiningTxFee:                  1e-8,
+			MaxStdTxValidationDuration:      3,
+			MaxNonStdTxValidationDuration:   1000,
+			MaxTxChainValidationBudget:      50,
+			ValidationClockCpu:              true,
+			MinConsolidationFactor:          20,
+			MaxConsolidationInputScriptSize: 150,
+			MinConfConsolidationInput:       6,
+			MinConsolidationInputMaturity:   6,
+			AcceptNonStdConsolidationInput:  false,
+		},
+	}
+}
+
+func getK8sWatcherConfig() *K8sWatcherConfig {
+	return &K8sWatcherConfig{
+		Namespace: "arc-testnetj",
+	}
+}
+
+func getDbConfig(dbName string) *DbConfig {
+	return &DbConfig{
+		Mode: "postgres",
+		Postgres: &PostgresConfig{
+			Host:         "localhost",
+			Port:         5432,
+			Name:         dbName,
+			User:         "arc",
+			Password:     "arc",
+			MaxIdleConns: 10,
+			MaxOpenConns: 80,
+			SslMode:      "disable",
+		},
 	}
 }

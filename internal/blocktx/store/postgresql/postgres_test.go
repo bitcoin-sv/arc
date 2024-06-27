bcba4ce795ef4c2bef98de824275ee7f529a6293
Arkadiusz Osowski <arkadiusz.osowski@4chain.studio>
2024-06-27 14:07:15 +0200

    ARCO-126: drop blocks.inserted_at_num

--- internal/blocktx/store/postgresql/postgres_test.go
+++ internal/blocktx/store/postgresql/postgres_test.go
@@ -30,19 +30,18 @@ import (
 )
 
 type Block struct {
-	ID            int64     `db:"id"`
-	Hash          string    `db:"hash"`
-	PreviousHash  string    `db:"prevhash"`
-	MerkleRoot    string    `db:"merkleroot"`
-	MerklePath    *string   `db:"merkle_path"`
-	Height        int64     `db:"height"`
-	Orphaned      bool      `db:"orphanedyn"`
-	Size          *int64    `db:"size"`
-	TxCount       *int64    `db:"tx_count"`
-	Processed     bool      `db:"processed"`
-	ProcessedAt   time.Time `db:"processed_at"`
-	InsertedAt    time.Time `db:"inserted_at"`
-	InsertedAtNum int64     `db:"inserted_at_num"`
+	ID           int64     `db:"id"`
+	Hash         string    `db:"hash"`
+	PreviousHash string    `db:"prevhash"`
+	MerkleRoot   string    `db:"merkleroot"`
+	MerklePath   *string   `db:"merkle_path"`
+	Height       int64     `db:"height"`
+	Orphaned     bool      `db:"orphanedyn"`
+	Size         *int64    `db:"size"`
+	TxCount      *int64    `db:"tx_count"`
+	Processed    bool      `db:"processed"`
+	ProcessedAt  time.Time `db:"processed_at"`
+	InsertedAt   time.Time `db:"inserted_at"`
 }
 
 type Transaction struct {

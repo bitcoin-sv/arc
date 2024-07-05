a2fc0a7a72e2dee3494af477006c200aa8ead041
kameshraj23 <kamesh.raj@taal.com>
2024-07-05 09:27:48 +0100

    refactor for top up

--- cmd/broadcaster-cli/app/keyset/topup/topup.go
+++ cmd/broadcaster-cli/app/keyset/topup/topup.go
@@ -4,24 +4,21 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log/slog"
+	"strings"
+	"time"
+
 	"github.com/bitcoin-sv/arc/cmd/broadcaster-cli/helper"
 	"github.com/bitcoin-sv/arc/internal/woc_client"
+	"github.com/bitcoin-sv/arc/pkg/keyset"
 	"github.com/spf13/cobra"
-	"log/slog"
+	"github.com/spf13/viper"
 )
 
 var Cmd = &cobra.Command{
 	Use:   "topup",
 	Short: "Top up funding address with BSV",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		keyFile, err := helper.GetString("keyFile")
-		if err != nil {
-			return err
-		}
-		if keyFile == "" {
-			return errors.New("no key file given")
-		}
-
 		isTestnet, err := helper.GetBool("testnet")
 		if err != nil {
 			return err
@@ -30,19 +27,72 @@ var Cmd = &cobra.Command{
 		if err != nil {
 			return err
 		}
+
 		logger := helper.GetLogger()
 
 		wocClient := woc_client.New(woc_client.WithAuth(wocApiKey), woc_client.WithLogger(logger))
-		fundingKeySet, _, err := helper.GetKeySetsKeyFile(keyFile)
-		if err != nil {
-			return fmt.Errorf("failed to get key sets: %v", err)
-		}
+		// fundingKeySet, _, err := helper.GetKeySetsKeyFile(keyFile)
+		// if err != nil {
+		// 	return fmt.Errorf("failed to get key sets: %v", err)
+		// }
 
-		err = wocClient.TopUp(context.Background(), !isTestnet, fundingKeySet.Address(!isTestnet))
+		// err = wocClient.TopUp(context.Background(), !isTestnet, fundingKeySet.Address(!isTestnet))
+		// if err != nil {
+		// 	return err
+		// }
+		// logger.Info("top up complete", slog.String("address", fundingKeySet.Address(!isTestnet)))
+
+		// return nil
+
+		keysFlag := viper.GetString("keys")
+		selectedKeys := strings.Split(keysFlag, ",")
+
+		var keys map[string]string
+		err = viper.UnmarshalKey("privateKeys", &keys)
 		if err != nil {
 			return err
 		}
-		logger.Info("top up complete", slog.String("address", fundingKeySet.Address(!isTestnet)))
+
+		var keySets []*keyset.KeySet
+
+		if len(keys) > 0 {
+			if len(selectedKeys) > 0 {
+				for _, selectedKey := range selectedKeys {
+					key, found := keys[selectedKey]
+					if !found {
+						return fmt.Errorf("key not found: %s", selectedKey)
+					}
+					fundingKeySet, _, err := helper.GetKeySetsXpriv(key)
+					if err != nil {
+						return fmt.Errorf("failed to get key sets: %v", err)
+					}
+					keySets = append(keySets, fundingKeySet)
+				}
+			} else {
+				for _, key := range keys {
+					fundingKeySet, _, err := helper.GetKeySetsXpriv(key)
+					if err != nil {
+						return fmt.Errorf("failed to get key sets: %v", err)
+					}
+					keySets = append(keySets, fundingKeySet)
+				}
+			}
+		} else {
+			return errors.New("no keys given in configuration")
+		}
+
+		for _, keySet := range keySets {
+
+			if wocApiKey == "" {
+				time.Sleep(500 * time.Millisecond)
+			}
+			err = wocClient.TopUp(context.Background(), !isTestnet, keySet.Address(!isTestnet))
+
+			if err != nil {
+				return err
+			}
+			logger.Info("top up complete", slog.String("address", keySet.Address(!isTestnet)))
+		}
 
 		return nil
 	},

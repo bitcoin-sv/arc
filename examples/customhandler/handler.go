4771878171a76b6e5538947bd6b43dc5ccb89273
Arkadiusz Osowski <arkadiusz.osowski@4chain.studio>
2024-07-18 10:51:04 +0200

    ARCO-155: update custom handler

--- examples/customhandler/handler.go
+++ examples/customhandler/handler.go
@@ -15,7 +15,7 @@ import (
 // CustomHandler is our custom arc handler
 // Define a custom handler, that overwrites the policy request, but uses other arc requests as is
 type CustomHandler struct {
-	handler.ArcDefaultHandler
+	h           handler.ArcDefaultHandler
 	MyCustomVar string `json:"my_custom_var"`
 }
 
@@ -46,24 +46,32 @@ func NewCustomHandler() (api.ServerInterface, error) {
 		nil,
 	)
 
+	// create custom handler
 	bitcoinHandler := &CustomHandler{
-		ArcDefaultHandler: *defaultHandler,
+		h: *defaultHandler,
 	}
 
 	return bitcoinHandler, nil
 }
-
-// GetArcV1Policy our custom policy request handler
-func (c *CustomHandler) GetArcV1Policy(ctx echo.Context) error {
+func (c *CustomHandler) GETPolicy(ctx echo.Context) error {
+	// custom get policy logic
 	arcPolicy := bitcoin.Settings{}
 
-	//
-	// you can use c.Client.Datastore()... to access the database
-	//
-	// or run a query directly against the db using gorm
-	//
-	// db := c.Client.Datastore().Execute("SELECT ....")
-	//
-
 	return ctx.JSON(http.StatusOK, arcPolicy)
+}
+
+func (c *CustomHandler) GETHealth(ctx echo.Context) error {
+	return c.h.GETHealth(ctx)
+}
+
+func (c *CustomHandler) POSTTransaction(ctx echo.Context, params api.POSTTransactionParams) error {
+	return c.h.POSTTransaction(ctx, params)
+}
+
+func (c *CustomHandler) GETTransactionStatus(ctx echo.Context, txid string) error {
+	return c.h.GETTransactionStatus(ctx, txid)
+}
+
+func (c *CustomHandler) POSTTransactions(ctx echo.Context, params api.POSTTransactionsParams) error {
+	return c.h.POSTTransactions(ctx, params)
 }

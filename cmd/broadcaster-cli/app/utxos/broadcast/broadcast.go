51150bce14dbcdfe178db0e72e245fea46c72d3e
Michael Böckli <michael.boeckli@gmail.com>
2024-07-18 15:20:47 +0200

    go mod tidy

--- cmd/broadcaster-cli/app/utxos/broadcast/broadcast.go
+++ cmd/broadcaster-cli/app/utxos/broadcast/broadcast.go
@@ -10,8 +10,8 @@ import (
 
 	"github.com/bitcoin-sv/arc/cmd/broadcaster-cli/helper"
 	"github.com/bitcoin-sv/arc/internal/broadcaster"
+	"github.com/bitcoin-sv/arc/internal/metamorph/metamorph_api"
 	"github.com/bitcoin-sv/arc/internal/woc_client"
-	"github.com/bitcoin-sv/arc/pkg/metamorph/metamorph_api"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )

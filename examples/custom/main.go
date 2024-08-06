c12073932552ead7e67b85e3d978e68433566ff5
Paweł Lewandowski <plewandowski555@gmail.com>
2024-08-06 13:58:20 +0200

    ARCO-163: replace deprecated oapi codegen middleware

--- examples/custom/main.go
+++ examples/custom/main.go
@@ -15,10 +15,10 @@ import (
 	apiHandler "github.com/bitcoin-sv/arc/pkg/api/handler"
 	"github.com/bitcoin-sv/arc/pkg/blocktx"
 	"github.com/bitcoin-sv/arc/pkg/metamorph"
-	"github.com/deepmap/oapi-codegen/pkg/middleware"
 	"github.com/getkin/kin-openapi/openapi3filter"
 	"github.com/labstack/echo/v4"
 	echomiddleware "github.com/labstack/echo/v4/middleware"
+	middleware "github.com/oapi-codegen/echo-middleware"
 )
 
 // This example does not use the configuration files or env variables,

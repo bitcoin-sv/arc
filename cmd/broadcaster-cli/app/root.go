c1f52d766a4067a53e4fa2a30d7a84fffa2d2260
Michael Böckli <michael.boeckli@gmail.com>
2024-08-08 10:32:48 +0200

    ARCO-130: Allow alternative configuration file

--- cmd/broadcaster-cli/app/root.go
+++ cmd/broadcaster-cli/app/root.go
@@ -2,21 +2,27 @@ package app
 
 import (
 	"errors"
-	"log"
-
 	"github.com/bitcoin-sv/arc/cmd/broadcaster-cli/app/keyset"
 	"github.com/bitcoin-sv/arc/cmd/broadcaster-cli/app/utxos"
+	"github.com/bitcoin-sv/arc/cmd/broadcaster-cli/helper"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"golang.org/x/sys/unix"
+	"log"
+	"log/slog"
+	"os"
 )
 
-var RootCmd = &cobra.Command{
-	Use:   "broadcaster",
-	Short: "CLI tool to broadcast transactions to ARC",
-}
+var (
+	logger  *slog.Logger
+	RootCmd = &cobra.Command{
+		Use:   "broadcaster",
+		Short: "CLI tool to broadcast transactions to ARC",
+	}
+)
 
 func init() {
+	logger = helper.GetLogger()
 	var err error
 	RootCmd.PersistentFlags().Bool("testnet", false, "Use testnet")
 	err = viper.BindPFlag("testnet", RootCmd.PersistentFlags().Lookup("testnet"))
@@ -36,10 +42,25 @@ func init() {
 		log.Fatal(err)
 	}
 
-	viper.AddConfigPath(".")
-	viper.AddConfigPath("./cmd/broadcaster-cli/")
-	viper.SetConfigName("broadcaster-cli")
-	// Todo: Allow setting alternative config file name with flag: e.g. --config=brc-config.yaml
+	var configFilenameArg string
+	args := os.Args
+	for i, arg := range args {
+		if arg == "-c" || arg == "--config" {
+			configFilenameArg = args[i+1]
+		}
+	}
+
+	if configFilenameArg != "" {
+		viper.SetConfigFile(configFilenameArg)
+	} else {
+		viper.AddConfigPath(".")
+		viper.AddConfigPath("./cmd/broadcaster-cli/")
+		viper.SetConfigName("broadcaster-cli")
+	}
+
+	if viper.ConfigFileUsed() != "" {
+		logger.Info("Config file used", slog.String("filename", viper.ConfigFileUsed()))
+	}
 
 	err = viper.ReadInConfig()
 	if err != nil {

087f8c17ebec2dd83683a7e8e218eaba83d6e1ef
kuba-4chain <jakub.smolka@4chain.studio>
2024-08-09 16:02:04 +0200

    feat: add unit tests for tricky situations in double spend statuses handling

--- internal/metamorph/processor_helpers_test.go
+++ internal/metamorph/processor_helpers_test.go
@@ -3,6 +3,10 @@ package metamorph
 import (
 	"fmt"
 	"testing"
+
+	"github.com/bitcoin-sv/arc/internal/metamorph/metamorph_api"
+	"github.com/bitcoin-sv/arc/internal/metamorph/store"
+	"github.com/stretchr/testify/assert"
 )
 
 func BenchmarkUnorderedEqual(b *testing.B) {
@@ -22,6 +26,77 @@ func BenchmarkUnorderedEqual(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				_ = unorderedEqual(txIDs, txIDs2)
 			}
+		})
+	}
+}
+
+func TestShouldUpdateStatus(t *testing.T) {
+	testCases := []struct {
+		name           string
+		existingStatus store.UpdateStatus
+		newStatus      store.UpdateStatus
+		expectedResult bool
+	}{
+		{
+			name: "new status lower than existing",
+			existingStatus: store.UpdateStatus{
+				Status: metamorph_api.Status_SEEN_ON_NETWORK,
+			},
+			newStatus: store.UpdateStatus{
+				Status: metamorph_api.Status_ACCEPTED_BY_NETWORK,
+			},
+			expectedResult: false,
+		},
+		{
+			name: "new status higher than existing",
+			existingStatus: store.UpdateStatus{
+				Status: metamorph_api.Status_ACCEPTED_BY_NETWORK,
+			},
+			newStatus: store.UpdateStatus{
+				Status: metamorph_api.Status_SEEN_ON_NETWORK,
+			},
+			expectedResult: true,
+		},
+		{
+			name: "new status lower than existing, unequal competing txs",
+			existingStatus: store.UpdateStatus{
+				Status: metamorph_api.Status_SEEN_ON_NETWORK,
+			},
+			newStatus: store.UpdateStatus{
+				Status:       metamorph_api.Status_ACCEPTED_BY_NETWORK,
+				CompetingTxs: []string{"1234"},
+			},
+			expectedResult: false,
+		},
+		{
+			name: "statuses equal",
+			existingStatus: store.UpdateStatus{
+				Status: metamorph_api.Status_SEEN_ON_NETWORK,
+			},
+			newStatus: store.UpdateStatus{
+				Status: metamorph_api.Status_SEEN_ON_NETWORK,
+			},
+			expectedResult: false,
+		},
+		{
+			name: "statuses equal, but unequal competing txs",
+			existingStatus: store.UpdateStatus{
+				Status:       metamorph_api.Status_DOUBLE_SPEND_ATTEMPTED,
+				CompetingTxs: []string{"5678"},
+			},
+			newStatus: store.UpdateStatus{
+				Status:       metamorph_api.Status_DOUBLE_SPEND_ATTEMPTED,
+				CompetingTxs: []string{"1234"},
+			},
+			expectedResult: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := shouldUpdateStatus(tc.newStatus, tc.existingStatus)
+
+			assert.Equal(t, tc.expectedResult, result)
 		})
 	}
 }

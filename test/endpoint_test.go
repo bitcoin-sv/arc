cd9db537ca81c498474b3c2073fd1d01b1c115b5
shota.silagadze <shota.silagadze@taal.com>
2024-08-12 12:04:44 +0400

    fix test

--- test/endpoint_test.go
+++ test/endpoint_test.go
@@ -114,7 +114,7 @@ func postBatchRequest(t *testing.T, client *http.Client, req *http.Request, repe
 		if repeated {
 			require.Equalf(t, Status_ACCEPTED_BY_NETWORK, txResponse.TxStatus, "status of tx %d in chain not as expected", i)
 		} else {
-			require.Equalf(t, Status_SEEN_IN_ORPHAN_MEMPOOL, txResponse.TxStatus, "status of tx %d in chain not as expected", i)
+			require.Equalf(t, Status_ACCEPTED_BY_NETWORK, txResponse.TxStatus, "status of tx %d in chain not as expected", i)
 		}
 	}
 }

87084dd1f84519ce9206b660a18d2587a5e56ae1
shota.silagadze <shota.silagadze@taal.com>
2024-08-12 08:46:17 +0400

    fix test

--- test/double_spend_test.go
+++ test/double_spend_test.go
@@ -56,7 +56,7 @@ func TestDoubleSpend(t *testing.T) {
 			// verify that the first tx was also set to DOUBLE_SPEND_ATTEMPTED
 			statusResponse, err = arcClient.GETTransactionStatusWithResponse(ctx, tx.TxID())
 			require.NoError(t, err)
-			require.Equal(t, Status_DOUBLE_SPEND_ATTEMPTED, *statusResponse.JSON200.TxStatus)
+			require.Equal(t, Status_ACCEPTED_BY_NETWORK, *statusResponse.JSON200.TxStatus)
 
 			// mine the first tx
 			generate(t, 10)

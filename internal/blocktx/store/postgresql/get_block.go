8e789ef501e0447ced765d75c582e2adc3cf27e1
shotasilagadzetaal <139438093+shotasilagadzetaal@users.noreply.github.com>
2024-08-13 17:41:05 +0200

    Staging (#514)
    Move blocktx tables into its schema
    
    Co-authored-by: ssilagadze <ssilagadze@purestorage.com>

--- internal/blocktx/store/postgresql/get_block.go
+++ internal/blocktx/store/postgresql/get_block.go
@@ -19,7 +19,7 @@ func (p *PostgreSQL) GetBlock(ctx context.Context, hash *chainhash.Hash) (*block
 		,b.height
 		,b.processed_at
 		,b.orphanedyn
-		FROM blocks b
+		FROM blocktx.blocks b
 		WHERE b.hash = $1
 	`
 

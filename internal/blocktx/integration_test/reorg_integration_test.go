44479729777032d03ce24c2678609044244754ca
kuba-4chain <jakub.smolka@4chain.studio>
2024-10-10 18:02:43 +0200

    feat: integration test for reorg - stale blocks

--- internal/blocktx/integration_test/reorg_integration_test.go
+++ internal/blocktx/integration_test/reorg_integration_test.go
@@ -26,19 +26,22 @@ import (
 	"testing"
 	"time"
 
-	_ "github.com/golang-migrate/migrate/v4/source/file"
-	_ "github.com/lib/pq"
-	"github.com/libsv/go-p2p/chaincfg/chainhash"
-	"github.com/libsv/go-p2p/wire"
-	"github.com/ory/dockertest/v3"
-	"github.com/stretchr/testify/require"
-
 	"github.com/bitcoin-sv/arc/internal/blocktx"
 	blockchain "github.com/bitcoin-sv/arc/internal/blocktx/blockchain_communication"
 	blocktx_p2p "github.com/bitcoin-sv/arc/internal/blocktx/blockchain_communication/p2p"
 	"github.com/bitcoin-sv/arc/internal/blocktx/blocktx_api"
 	"github.com/bitcoin-sv/arc/internal/blocktx/store/postgresql"
+	"github.com/bitcoin-sv/arc/internal/message_queue/nats/client/nats_core"
+	nats_mock "github.com/bitcoin-sv/arc/internal/message_queue/nats/client/nats_core/mocks"
 	testutils "github.com/bitcoin-sv/arc/internal/test_utils"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
+	"github.com/libsv/go-bc"
+	"github.com/libsv/go-p2p/chaincfg/chainhash"
+	"github.com/libsv/go-p2p/wire"
+	"github.com/ory/dockertest/v3"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
 )
 
 const (
@@ -89,7 +92,7 @@ func testmain(m *testing.M) int {
 	return m.Run()
 }
 
-func TestBlockStatus(t *testing.T) {
+func TestReorg(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping integration test")
 	}
@@ -104,12 +107,49 @@ func TestBlockStatus(t *testing.T) {
 	blocktxStore, err := postgresql.New(dbInfo, 10, 80)
 	require.NoError(t, err)
 
+	publishedTxs := make([]*blocktx_api.TransactionBlock, 0)
+
+	mockNatsConn := &nats_mock.NatsConnectionMock{
+		PublishFunc: func(subj string, data []byte) error {
+			serialized := &blocktx_api.TransactionBlock{}
+			err := proto.Unmarshal(data, serialized)
+			require.NoError(t, err)
+
+			publishedTxs = append(publishedTxs, serialized)
+			return nil
+		},
+	}
+	mqClient := nats_core.New(mockNatsConn, nats_core.WithLogger(logger))
+
 	p2pMsgHandler := blocktx_p2p.NewMsgHandler(logger, blockRequestCh, blockProcessCh)
-	processor, err := blocktx.NewProcessor(logger, blocktxStore, blockRequestCh, blockProcessCh)
+	processor, err := blocktx.NewProcessor(
+		logger,
+		blocktxStore,
+		blockRequestCh,
+		blockProcessCh,
+		blocktx.WithMessageQueueClient(mqClient),
+	)
 	require.NoError(t, err)
 
 	processor.StartBlockProcessing()
 
+	testHandleBlockOnEmptyDatabase(t, p2pMsgHandler, blocktxStore)
+	publishedTxs = make([]*blocktx_api.TransactionBlock, 0) // clear slice for the next test
+
+	// only load fixtures at this point
+	testutils.LoadFixtures(t, dbConn, "fixtures")
+
+	staleBlockHash, expectedTxs := testHandleStaleBlock(t, p2pMsgHandler, blocktxStore, publishedTxs)
+	// verify the transaction was correctly published to metamorph
+	verifyTxs(t, expectedTxs, publishedTxs)
+	// clear slice for the next test
+	publishedTxs = publishedTxs[:0]
+
+	expectedTxs = testHandleReorg(t, p2pMsgHandler, blocktxStore, publishedTxs, staleBlockHash)
+	verifyTxs(t, expectedTxs, publishedTxs)
+}
+
+func testHandleBlockOnEmptyDatabase(t *testing.T, p2pMsgHandler *blocktx_p2p.MsgHandler, store *postgresql.PostgreSQL) {
 	// test for empty database edge case before inserting fixtures
 	prevBlockHash := testutils.RevChainhash(t, "00000000000000000a00c377b260a3219b0c314763f486bc363df7aa7e22ad72")
 	txHash, err := chainhash.NewHashFromStr("be181e91217d5f802f695e52144078f8dfbe51b8a815c3d6fb48c0d853ec683b")
@@ -136,22 +176,21 @@ func TestBlockStatus(t *testing.T) {
 
 	blockHashZero := blockMessage.Header.BlockHash()
 
-	block, err := blocktxStore.GetBlock(context.Background(), &blockHashZero)
+	block, err := store.GetBlock(context.Background(), &blockHashZero)
 	require.NoError(t, err)
 	require.Equal(t, uint64(822011), block.Height)
 	require.Equal(t, blocktx_api.Status_LONGEST, block.Status)
+}
 
-	// only load fixtures at this point
-	testutils.LoadFixtures(t, dbConn, "fixtures")
-
-	prevBlockHash = testutils.RevChainhash(t, "f97e20396f02ab990ed31b9aec70c240f48b7e5ea239aa050000000000000000")
-	txHash, err = chainhash.NewHashFromStr("be181e91217d5f802f695e52144078f8dfbe51b8a815c3d6fb48c0d853ec683b")
-	require.NoError(t, err)
-	merkleRoot, err = chainhash.NewHashFromStr("be181e91217d5f802f695e52144078f8dfbe51b8a815c3d6fb48c0d853ec683b")
-	require.NoError(t, err)
+func testHandleStaleBlock(t *testing.T, p2pMsgHandler *blocktx_p2p.MsgHandler, store *postgresql.PostgreSQL, publishedTxs []*blocktx_api.TransactionBlock) (*chainhash.Hash, []*blocktx_api.TransactionBlock) {
+	prevBlockHash := testutils.RevChainhash(t, "f97e20396f02ab990ed31b9aec70c240f48b7e5ea239aa050000000000000000")
+	txHash := testutils.RevChainhash(t, "cd3d2f97dfc0cdb6a07ec4b72df5e1794c9553ff2f62d90ed4add047e8088853")
+	txHash2 := testutils.RevChainhash(t, "b16cea53fc823e146fbb9ae4ad3124f7c273f30562585ad6e4831495d609f430") // should not be published - is already in the longest chain
+	treeStore := bc.BuildMerkleTreeStoreChainHash([]*chainhash.Hash{txHash, txHash2})
+	merkleRoot := treeStore[len(treeStore)-1]
 
 	// should become STALE
-	blockMessage = &blockchain.BlockMessage{
+	blockMessage := &blockchain.BlockMessage{
 		Header: &wire.BlockHeader{
 			Version:    541065216,
 			PrevBlock:  *prevBlockHash, // block with status LONGEST at height 822014
@@ -159,27 +198,42 @@ func TestBlockStatus(t *testing.T) {
 			Bits:       0x1d00ffff, // chainwork: "4295032833" lower than the competing block
 		},
 		Height:            uint64(822015), // competing block already exists at this height
-		TransactionHashes: []*chainhash.Hash{txHash},
+		TransactionHashes: []*chainhash.Hash{txHash, txHash2},
 	}
 
 	p2pMsgHandler.OnReceive(blockMessage, nil)
-	require.NoError(t, err)
 	// Allow DB to process the block
 	time.Sleep(200 * time.Millisecond)
 
 	blockHashStale := blockMessage.Header.BlockHash()
 
-	block, err = blocktxStore.GetBlock(context.Background(), &blockHashStale)
+	block, err := store.GetBlock(context.Background(), &blockHashStale)
 	require.NoError(t, err)
 	require.Equal(t, uint64(822015), block.Height)
 	require.Equal(t, blocktx_api.Status_STALE, block.Status)
 
+	// transactions expected to be published to metamorph
+	expectedTxs := []*blocktx_api.TransactionBlock{
+		{
+			BlockHash:       blockHashStale[:],
+			BlockHeight:     822015,
+			TransactionHash: txHash[:],
+		},
+	}
+
+	return &blockHashStale, expectedTxs
+}
+
+func testHandleReorg(t *testing.T, p2pMsgHandler *blocktx_p2p.MsgHandler, store *postgresql.PostgreSQL, publishedTxs []*blocktx_api.TransactionBlock, staleBlockHash *chainhash.Hash) []*blocktx_api.TransactionBlock {
+	txHash := testutils.RevChainhash(t, "2ff4430eb883c6f6c0640a5d716b2d107bbc0efa5aeaa237aec796d4686b0a8f")
+	merkleRoot := testutils.RevChainhash(t, "2ff4430eb883c6f6c0640a5d716b2d107bbc0efa5aeaa237aec796d4686b0a8f")
+
 	// should become LONGEST
 	// reorg should happen
-	blockMessage = &blockchain.BlockMessage{
+	blockMessage := &blockchain.BlockMessage{
 		Header: &wire.BlockHeader{
 			Version:    541065216,
-			PrevBlock:  blockHashStale, // block with status STALE at height 822015
+			PrevBlock:  *staleBlockHash, // block with status STALE at height 822015
 			MerkleRoot: *merkleRoot,
 			Bits:       0x1a05db8b, // chainwork: "12301577519373468" higher than the competing block
 		},
@@ -188,44 +242,80 @@ func TestBlockStatus(t *testing.T) {
 	}
 
 	p2pMsgHandler.OnReceive(blockMessage, nil)
-	require.NoError(t, err)
 	// Allow DB to process the block and perform reorg
 	time.Sleep(1 * time.Second)
 
 	// verify that reorg happened
 	blockHashLongest := blockMessage.Header.BlockHash()
 
-	block, err = blocktxStore.GetBlock(context.Background(), &blockHashLongest)
+	block, err := store.GetBlock(context.Background(), &blockHashLongest)
 	require.NoError(t, err)
 	require.Equal(t, uint64(822016), block.Height)
 	require.Equal(t, blocktx_api.Status_LONGEST, block.Status)
 
-	block, err = blocktxStore.GetBlock(context.Background(), &blockHashStale)
+	block, err = store.GetBlock(context.Background(), staleBlockHash)
 	require.NoError(t, err)
 	require.Equal(t, uint64(822015), block.Height)
 	require.Equal(t, blocktx_api.Status_LONGEST, block.Status)
 
 	previouslyLongestBlockHash := testutils.RevChainhash(t, "c9b4e1e4dcf9188416027511671b9346be8ef93c0ddf59060000000000000000")
-	block, err = blocktxStore.GetBlock(context.Background(), previouslyLongestBlockHash)
+	block, err = store.GetBlock(context.Background(), previouslyLongestBlockHash)
 	require.NoError(t, err)
 	require.Equal(t, uint64(822015), block.Height)
 	require.Equal(t, blocktx_api.Status_STALE, block.Status)
 
 	previouslyLongestBlockHash = testutils.RevChainhash(t, "e1df1273e6e7270f96b508545d7aa80aebda7d758dc82e080000000000000000")
-	block, err = blocktxStore.GetBlock(context.Background(), previouslyLongestBlockHash)
+	block, err = store.GetBlock(context.Background(), previouslyLongestBlockHash)
 	require.NoError(t, err)
 	require.Equal(t, uint64(822016), block.Height)
 	require.Equal(t, blocktx_api.Status_STALE, block.Status)
 
 	previouslyLongestBlockHash = testutils.RevChainhash(t, "76404890880cb36ce68100abb05b3a958e17c0ed274d5c0a0000000000000000")
-	block, err = blocktxStore.GetBlock(context.Background(), previouslyLongestBlockHash)
+	block, err = store.GetBlock(context.Background(), previouslyLongestBlockHash)
 	require.NoError(t, err)
 	require.Equal(t, uint64(822017), block.Height)
 	require.Equal(t, blocktx_api.Status_STALE, block.Status)
 
 	beginningOfChain := testutils.RevChainhash(t, "f97e20396f02ab990ed31b9aec70c240f48b7e5ea239aa050000000000000000")
-	block, err = blocktxStore.GetBlock(context.Background(), beginningOfChain)
+	block, err = store.GetBlock(context.Background(), beginningOfChain)
 	require.NoError(t, err)
 	require.Equal(t, uint64(822014), block.Height)
 	require.Equal(t, blocktx_api.Status_LONGEST, block.Status)
+
+	expectedTxs := []*blocktx_api.TransactionBlock{
+		{
+			BlockHash:       staleBlockHash[:],
+			BlockHeight:     822015,
+			TransactionHash: testutils.RevChainhash(t, "cd3d2f97dfc0cdb6a07ec4b72df5e1794c9553ff2f62d90ed4add047e8088853")[:],
+		},
+		{
+			BlockHash:       staleBlockHash[:],
+			BlockHeight:     822015,
+			TransactionHash: testutils.RevChainhash(t, "b16cea53fc823e146fbb9ae4ad3124f7c273f30562585ad6e4831495d609f430")[:],
+		},
+		// {
+		// 	BlockHash:       blockHashLongest[:],
+		// 	BlockHeight:     822016,
+		// 	TransactionHash: txHash[:],
+		// },
+	}
+
+	return expectedTxs
+}
+
+func verifyTxs(t *testing.T, expectedTxs []*blocktx_api.TransactionBlock, publishedTxs []*blocktx_api.TransactionBlock) {
+	strippedTxs := make([]*blocktx_api.TransactionBlock, len(publishedTxs))
+	for i, tx := range publishedTxs {
+		chash, err := chainhash.NewHash(tx.TransactionHash)
+		require.NoError(t, err)
+		t.Logf("published tx hash: %s", chash.String())
+		strippedTxs[i] = &blocktx_api.TransactionBlock{
+			BlockHash:       tx.BlockHash,
+			BlockHeight:     tx.BlockHeight,
+			TransactionHash: tx.TransactionHash,
+			// TODO: add block status
+		}
+	}
+
+	require.ElementsMatch(t, expectedTxs, strippedTxs)
 }
--- internal/blocktx/processor.go
+++ internal/blocktx/processor.go
@@ -478,6 +478,7 @@ func (p *Processor) processBlock(msg *blockchain.BlockMessage) (err error) {
 	shouldPerformReorg := false
 	if competing {
 		p.logger.Info("Competing blocks found", slog.String("incoming block hash", blockHash.String()), slog.Uint64("height", incomingBlock.Height))
+		incomingBlock.Status = blocktx_api.Status_STALE
 
 		hasGreatestChainwork, err := p.hasGreatestChainwork(ctx, incomingBlock)
 		if err != nil {
@@ -485,13 +486,8 @@ func (p *Processor) processBlock(msg *blockchain.BlockMessage) (err error) {
 			return err
 		}
 
-		incomingBlock.Status = blocktx_api.Status_STALE
-
 		if hasGreatestChainwork {
 			p.logger.Info("chain reorg detected", slog.String("hash", blockHash.String()), slog.Uint64("height", incomingBlock.Height))
-
-			incomingBlock.Status = blocktx_api.Status_LONGEST
-
 			shouldPerformReorg = true
 		}
 	}
@@ -545,6 +541,8 @@ func (p *Processor) processBlock(msg *blockchain.BlockMessage) (err error) {
 			TransactionHash: tx.TxHash,
 			MerklePath:      tx.MerklePath,
 		}
+
+		p.logger.Info("publishing tx", slog.String("txHash", getHashStringNoErr(tx.TxHash)))
 
 		// change that receiver method in metamorph to accept statuses (MINED and MINED_IN_STALE_BLOCK)
 		err = p.mqClient.PublishMarshal(MinedTxsTopic, txBlock)
@@ -629,7 +627,7 @@ func (p *Processor) hasGreatestChainwork(ctx context.Context, incomingBlock *blo
 }
 
 func (p *Processor) performReorg(ctx context.Context, incomingBlock *blocktx_api.Block) ([]store.TransactionBlock, error) {
-	staleBlocks, err := p.store.GetStaleChainBackFromHash(ctx, incomingBlock.PreviousHash)
+	staleBlocks, err := p.store.GetStaleChainBackFromHash(ctx, incomingBlock.Hash)
 	if err != nil {
 		return nil, err
 	}
@@ -669,6 +667,8 @@ func (p *Processor) performReorg(ctx context.Context, incomingBlock *blocktx_api
 	if err != nil {
 		return nil, err
 	}
+
+	p.logger.Info("reorg performed successfully")
 
 	prevLongestTxs := make([]store.TransactionBlock, 0)
 	prevStaleTxs := make([]store.TransactionBlock, 0)
@@ -751,7 +751,7 @@ func (p *Processor) storeTransactions(ctx context.Context, blockId uint64, block
 
 		if percentage, found := progress[txIndex+1]; found {
 			if totalSize > 0 {
-				p.logger.Info(fmt.Sprintf("%d txs out of %d marked as mined", txIndex+1, totalSize), slog.Int("percentage", percentage), slog.String("hash", blockhash.String()), slog.Uint64("height", block.Height), slog.String("duration", time.Since(now).String()))
+				p.logger.Info(fmt.Sprintf("%d txs out of %d stored", txIndex+1, totalSize), slog.Int("percentage", percentage), slog.String("hash", blockhash.String()), slog.Uint64("height", block.Height), slog.String("duration", time.Since(now).String()))
 			}
 		}
 	}

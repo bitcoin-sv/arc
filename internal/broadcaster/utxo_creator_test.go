ff26fc8325d2afbf6863e2339603400fa417d454
kameshraj23 <kamesh.raj@taal.com>
2024-10-15 09:36:58 +0100

    ARCO-153: removing existing test nad updating new test

--- internal/broadcaster/utxo_creator_test.go
+++ internal/broadcaster/utxo_creator_test.go
@@ -1,83 +1,110 @@
 package broadcaster_test
 
 import (
+	"context"
 	"errors"
+	"log/slog"
+	"os"
 	"testing"
+	"time"
 
+	"github.com/bitcoin-sv/arc/internal/broadcaster"
 	"github.com/bitcoin-sv/arc/internal/broadcaster/mocks"
+	"github.com/bitcoin-sv/arc/internal/metamorph/metamorph_api"
+	"github.com/bitcoin-sv/arc/internal/testdata"
+	"github.com/bitcoin-sv/arc/pkg/keyset"
+	"github.com/bitcoin-sv/go-sdk/script"
+	sdkTx "github.com/bitcoin-sv/go-sdk/transaction"
 	"github.com/stretchr/testify/require"
 )
 
 func TestUTXOCreator(t *testing.T) {
-	tt := []struct {
-		name               string
-		outputs            int
-		satoshisPerOutput  uint64
-		startFunc          func(outputs int, satoshisPerOutput uint64) error
-		expectedError      error
-		expectedStartCalls int
+	ks, err := keyset.New()
+	require.NoError(t, err)
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	tests := []struct {
+		name                   string
+		getUTXOsResp           sdkTx.UTXOs
+		getBalanceFunc         func(ctx context.Context, address string, constantBackoff time.Duration, retries uint64) (int64, int64, error)
+		expectedBroadcastCalls int
+		expectedError          error
+		requestedUTXOs         int
+		requestedAmountPerUTXO uint64
 	}{
 		{
-			name:              "successful start",
-			outputs:           5,
-			satoshisPerOutput: 1000,
-			startFunc: func(outputs int, satoshisPerOutput uint64) error {
-				return nil
+			name:         "success - creates correct UTXOs",
+			getUTXOsResp: sdkTx.UTXOs{{TxID: testdata.TX1Hash[:], Vout: 0, LockingScript: ks.Script, Satoshis: 401}},
+			getBalanceFunc: func(ctx context.Context, address string, constantBackoff time.Duration, retries uint64) (int64, int64, error) {
+				return 400, 0, nil
 			},
-			expectedError:      nil,
-			expectedStartCalls: 1,
+			expectedBroadcastCalls: 1,
+			expectedError:          nil,
+			requestedUTXOs:         4,
+			requestedAmountPerUTXO: 100,
 		},
 		{
-			name:              "error on start",
-			outputs:           5,
-			satoshisPerOutput: 1000,
-			startFunc: func(outputs int, satoshisPerOutput uint64) error {
-				return errors.New("failed to start UTXO creation")
+			name:         "Insufficient balance",
+			getUTXOsResp: sdkTx.UTXOs{{TxID: testdata.TX1Hash[:], Vout: 0, LockingScript: ks.Script, Satoshis: 50}},
+			getBalanceFunc: func(ctx context.Context, address string, constantBackoff time.Duration, retries uint64) (int64, int64, error) {
+				return 100, 0, nil
 			},
-			expectedError:      errors.New("failed to start UTXO creation"),
-			expectedStartCalls: 1,
-		},
-		{
-			name:              "invalid input - zero outputs",
-			outputs:           0,
-			satoshisPerOutput: 1000,
-			startFunc: func(outputs int, satoshisPerOutput uint64) error {
-				return nil // Simulate success to test input handling
-			},
-			expectedError:      nil, // Start should handle zero outputs gracefully
-			expectedStartCalls: 1,
-		},
-		{
-			name:              "invalid input - zero satoshis per output",
-			outputs:           5,
-			satoshisPerOutput: 0,
-			startFunc: func(outputs int, satoshisPerOutput uint64) error {
-				return nil // Simulate success to test input handling
-			},
-			expectedError:      nil, // Start should handle zero satoshis gracefully
-			expectedStartCalls: 1,
+			expectedBroadcastCalls: 0,
+			expectedError:          errors.New("requested total of satoshis exceeds balance\nrequested: 400, balance: 100"),
+			requestedUTXOs:         4,
+			requestedAmountPerUTXO: 100,
 		},
 	}
 
-	for _, tc := range tt {
-		t.Run(tc.name, func(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
 			// Given
-			mockedCreator := &mocks.CreatorMock{
-				StartFunc: tc.startFunc,
+			mockArcClient := &mocks.ArcClientMock{
+				BroadcastTransactionsFunc: func(ctx context.Context, txs sdkTx.Transactions, waitForStatus metamorph_api.Status, callbackURL, callbackToken string, fullStatusUpdates, skipFeeValidation bool) ([]*metamorph_api.TransactionStatus, error) {
+					statuses := make([]*metamorph_api.TransactionStatus, len(txs))
+					for i, tx := range txs {
+						statuses[i] = &metamorph_api.TransactionStatus{Txid: tx.TxID(), Status: metamorph_api.Status_SEEN_ON_NETWORK}
+					}
+					return statuses, nil
+				},
 			}
+
+			mockUtxoClient := &mocks.UtxoClientMock{
+				GetBalanceWithRetriesFunc: tt.getBalanceFunc,
+				GetUTXOsWithRetriesFunc: func(ctx context.Context, lockingScript *script.Script, address string, constantBackoff time.Duration, retries uint64) (sdkTx.UTXOs, error) {
+					return tt.getUTXOsResp, nil
+				},
+			}
+
+			utxoCreator, err := broadcaster.NewUTXOCreator(logger, mockArcClient, ks, mockUtxoClient, false)
+			require.NoError(t, err)
+			defer utxoCreator.Shutdown()
+
 			// When
-			err := mockedCreator.Start(tc.outputs, tc.satoshisPerOutput)
+			actualError := utxoCreator.Start(tt.requestedUTXOs, tt.requestedAmountPerUTXO)
 
 			// Then
-			if tc.expectedError != nil {
-				require.Error(t, err)
-				require.EqualError(t, err, tc.expectedError.Error())
+			if tt.expectedError != nil {
+				require.EqualError(t, actualError, tt.expectedError.Error(), "Error message mismatch")
+				return
 			} else {
-				require.NoError(t, err)
+				require.NoError(t, actualError)
 			}
 
-			require.Equal(t, tc.expectedStartCalls, len(mockedCreator.StartCalls()))
+			time.Sleep(500 * time.Millisecond)
 
+			require.Equal(t, tt.expectedBroadcastCalls, len(mockArcClient.BroadcastTransactionsCalls()), "Unexpected number of BroadcastTransactions calls")
+
+			if tt.expectedBroadcastCalls > 0 {
+				for _, call := range mockArcClient.BroadcastTransactionsCalls() {
+					for _, tx := range call.Txs {
+						for _, output := range tx.Outputs {
+							require.Equal(t, tt.requestedAmountPerUTXO, output.Satoshis, "Each UTXO output should have the requested amount")
+						}
+					}
+				}
+			}
 		})
 	}
 }

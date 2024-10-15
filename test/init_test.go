77bfb8f5acc3bc4986b3a2619aea302b7bd81338
Arkadiusz Osowski <arkadiusz.osowski@4chain.studio>
2024-10-15 16:25:30 +0200

    ARCO-220: fix test init to ensure all blocks are processed by blocktx instances

--- test/init_test.go
+++ test/init_test.go
@@ -37,11 +37,32 @@ func setupSut() {
 	}
 
 	// fund node
-	if info.Blocks < 101 {
-		_, err = bitcoind.Generate(101)
-		if err != nil {
-			log.Fatalln(err)
+	const minNumbeOfBlocks = 101
+
+	if info.Blocks < minNumbeOfBlocks {
+		// generate blocks in part to make sure blocktx is able to process all blocks
+		const blockBatch = 4
+
+		for {
+			_, err = bitcoind.Generate(blockBatch)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			//give time to process blocks
+			time.Sleep(2 * time.Second)
+
+			info, err = bitcoind.GetInfo()
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			missingBlocks := minNumbeOfBlocks - info.Blocks
+			log.Println(missingBlocks)
+
+			if missingBlocks < 0 {
+				break
+			}
 		}
-		time.Sleep(5 * time.Second)
 	}
 }

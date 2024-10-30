58599c02f5b2fbfb3d6636517b0ec3eb2edc51fc
Paweł Lewandowski <plewandowski555@gmail.com>
2024-10-30 13:20:16 +0100

    ARCO-209: fix PR comments

--- pkg/blocktx/client_mocks.go
+++ pkg/blocktx/client_mocks.go
@@ -2,4 +2,4 @@ package blocktx
 
 //go:generate moq -pkg mocks -out ./mocks/merkle_roots_verifier_mock.go . MerkleRootsVerifier
 
-//go:generate moq -pkg mocks -out ./mocks/blocktx_client_mock.go . BlockClient
+//go:generate moq -pkg mocks -out ./mocks/blocktx_client_mock.go . Watcher

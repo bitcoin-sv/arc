76eab4e4579762ce474f1ca454f009e28b38bfe6
shota.silagadze <shota.silagadze@taal.com>
2024-11-12 14:04:34 +0400

    refactor(ARCO-282): Separate callbacker #642

--- internal/callbacker/callbacker_mocks.go
+++ internal/callbacker/callbacker_mocks.go
@@ -2,3 +2,5 @@ package callbacker
 
 // from callbacker.go
 //go:generate moq -out ./callbacker_mock.go ./ SenderI
+
+//go:generate moq -out ./callbacker_api/callbacker_api_client_mock.go ./callbacker_api/ CallbackerAPIClient
---
+++ pkg/callbacker/callbacker_test.go
@@ -0,0 +1,75 @@
+package callbacker
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"testing"
+
+	"github.com/bitcoin-sv/arc/internal/callbacker/callbacker_api"
+	"github.com/bitcoin-sv/arc/internal/metamorph/metamorph_api"
+	"github.com/bitcoin-sv/arc/internal/metamorph/store"
+	"github.com/libsv/go-p2p/chaincfg/chainhash"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func TestSendCallback(t *testing.T) {
+	tt := []struct {
+		name          string
+		expectedCalls int
+		err           error
+		data          *store.Data
+	}{
+		{
+			name:          "empty callbacks",
+			expectedCalls: 0,
+			data: &store.Data{
+				Status:    metamorph_api.Status_UNKNOWN,
+				Hash:      &chainhash.Hash{},
+				Callbacks: []store.Callback{},
+			},
+		},
+		{
+			name:          "empty url",
+			expectedCalls: 0,
+
+			data: &store.Data{
+				Status: metamorph_api.Status_UNKNOWN,
+				Hash:   &chainhash.Hash{},
+				Callbacks: []store.Callback{
+					{
+						CallbackURL: "",
+					},
+				},
+			},
+		},
+		{
+			name:          "expected call",
+			expectedCalls: 1,
+			data: &store.Data{
+				Status: metamorph_api.Status_UNKNOWN,
+				Hash:   &chainhash.Hash{},
+				Callbacks: []store.Callback{
+					{
+						CallbackURL: "http://someurl.comg",
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			apiClient := &callbacker_api.CallbackerAPIClientMock{
+				SendCallbackFunc: func(_ context.Context, _ *callbacker_api.SendCallbackRequest, _ ...grpc.CallOption) (*emptypb.Empty, error) {
+					return nil, nil
+				},
+			}
+			grpcCallbacker := NewGrpcCallbacker(apiClient, slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo})))
+			grpcCallbacker.SendCallback(context.Background(), tc.data)
+			require.Equal(t, tc.expectedCalls, len(apiClient.SendCallbackCalls()))
+		})
+	}
+}

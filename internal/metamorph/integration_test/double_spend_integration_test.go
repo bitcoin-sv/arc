63a68932dad235626c1bfc5795b256f8d7080229
Paweł Lewandowski <plewandowski555@gmail.com>
2024-11-13 09:32:11 +0100

    feat(ARCO-283): remove freecache, combine methods with cache implementation

--- internal/metamorph/integration_test/double_spend_integration_test.go
+++ internal/metamorph/integration_test/double_spend_integration_test.go
@@ -28,7 +28,6 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/bitcoin-sv/arc/internal/blocktx/blocktx_api"
-	"github.com/bitcoin-sv/arc/internal/cache"
 	"github.com/bitcoin-sv/arc/internal/metamorph"
 	"github.com/bitcoin-sv/arc/internal/metamorph/metamorph_api"
 	"github.com/bitcoin-sv/arc/internal/metamorph/mocks"
@@ -82,7 +81,7 @@ func TestDoubleSpendDetection(t *testing.T) {
 		require.NoError(t, err)
 	}
 
-	processor, err := metamorph.NewProcessor(metamorphStore, cStore, pm, statusMessageChannel,
+	processor, err := metamorph.NewProcessor(metamorphStore, nil, pm, statusMessageChannel,
 		metamorph.WithMinedTxsChan(minedTxChannel),
 		metamorph.WithNow(func() time.Time { return time.Date(2023, 10, 1, 13, 0, 0, 0, time.UTC) }),
 		metamorph.WithProcessStatusUpdatesInterval(200*time.Millisecond),

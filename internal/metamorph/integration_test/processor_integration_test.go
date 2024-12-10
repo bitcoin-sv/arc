ab97f805cfc032fd630f1396a4206562536b559f
kuba-4chain <jakub.smolka@4chain.studio>
2024-12-10 14:05:34 +0100

    feat: improve tests with final status case

--- internal/metamorph/integration_test/processor_integration_test.go
+++ internal/metamorph/integration_test/processor_integration_test.go
@@ -14,10 +14,16 @@ import (
 	"github.com/bitcoin-sv/arc/internal/metamorph/store"
 	"github.com/bitcoin-sv/arc/internal/metamorph/store/postgresql"
 	testutils "github.com/bitcoin-sv/arc/internal/test_utils"
+	"github.com/libsv/go-p2p"
+	"github.com/libsv/go-p2p/chaincfg/chainhash"
 	"github.com/stretchr/testify/require"
 )
 
 func TestProcessor(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+
 	t.Run("registered transactions in redis cache", func(t *testing.T) {
 		// given
 		mtmStore, err := postgresql.New(dbInfo, "txs-cache-integration-test", 10, 80)
@@ -25,7 +31,15 @@ func TestProcessor(t *testing.T) {
 
 		cacheStore := cache.NewRedisStore(context.Background(), redisClient)
 
-		pm := &mocks.PeerManagerMock{ShutdownFunc: func() {}}
+		pm := &mocks.PeerManagerMock{
+			ShutdownFunc: func() {},
+			RequestTransactionFunc: func(_ *chainhash.Hash) p2p.PeerI {
+				return nil
+			},
+			AnnounceTransactionFunc: func(_ *chainhash.Hash, _ []p2p.PeerI) []p2p.PeerI {
+				return nil
+			},
+		}
 		natsMock := &nats_mocks.NatsConnectionMock{
 			DrainFunc: func() error {
 				return nil
@@ -47,33 +61,55 @@ func TestProcessor(t *testing.T) {
 		sut.StartSendStatusUpdate()
 		sut.StartProcessStatusUpdatesInStorage()
 
+		const numberOfCacheTxs = 2
+
 		tx1 := testutils.RevChainhash(t, "830b8424653d2e2eaedfd802d37696821ee5f538a0837dd27ae817a20804b4c5")
 		tx2 := testutils.RevChainhash(t, "f00bf349d23b14ab23931e668312f2fe8e58024b462e3d038332581c1433e4a2")
+		txNotRegistered := testutils.RevChainhash(t, "acd4d7bf340e420abe925a63f0d6cf9310292106a8f396ac738a19ad5b9b3b63")
 
 		tx1ResponseChannel := make(chan metamorph.StatusAndError, 10)
-		req := &metamorph.ProcessorRequest{
+		tx2ResponseChannel := make(chan metamorph.StatusAndError, 10)
+
+		req1 := &metamorph.ProcessorRequest{
 			Data: &store.Data{
 				Hash:              tx1,
 				Status:            metamorph_api.Status_STORED,
 				FullStatusUpdates: false,
 			},
-			ResponseChannel: make(chan metamorph.StatusAndError),
+			ResponseChannel: tx1ResponseChannel,
+		}
+		req2 := &metamorph.ProcessorRequest{
+			Data: &store.Data{
+				Hash:              tx2,
+				Status:            metamorph_api.Status_STORED,
+				FullStatusUpdates: false,
+			},
+			ResponseChannel: tx2ResponseChannel,
 		}
 
 		// when
-		sut.ProcessTransaction(context.Background(), req)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		sut.ProcessTransaction(ctx, req1)
+		sut.ProcessTransaction(ctx, req2)
 
 		// then
-		tx1Bytes, err := cacheStore.MapGet(metamorph.CacheRegisteredTxsHash, tx1.String())
+		cacheTxs, err := cacheStore.MapGetAll(metamorph.CacheRegisteredTxsHash)
 		require.NoError(t, err)
-		require.NotNil(t, tx1Bytes)
+		require.NotNil(t, cacheTxs[tx1.String()])
+		require.NotNil(t, cacheTxs[tx2.String()])
+		require.Equal(t, numberOfCacheTxs, len(cacheTxs))
 
-		txCount, err := cacheStore.MapLen(metamorph.CacheRegisteredTxsHash)
-		require.NoError(t, err)
-		require.Equal(t, int64(1), txCount)
-
-		for r := range tx1ResponseChannel {
-			require.Equal(t, tx1.String(), r.Hash.String())
+	consumeStatuses:
+		for {
+			select {
+			case r1 := <-tx1ResponseChannel:
+				require.Equal(t, tx1.String(), r1.Hash.String())
+			case r2 := <-tx2ResponseChannel:
+				require.Equal(t, tx2.String(), r2.Hash.String())
+			default:
+				break consumeStatuses
+			}
 		}
 
 		// when
@@ -83,32 +119,34 @@ func TestProcessor(t *testing.T) {
 			Peer:   "",
 		}
 		statusMessageChannel <- &metamorph.TxStatusMessage{
-			Hash:   tx2, // this tx should not be processed
+			Hash:   tx2,
+			Status: metamorph_api.Status_REJECTED, // this tx should be removed from the cache - final status
+			Peer:   "",
+		}
+		statusMessageChannel <- &metamorph.TxStatusMessage{
+			Hash:   txNotRegistered, // this tx should not be processed
 			Status: metamorph_api.Status_ACCEPTED_BY_NETWORK,
 			Peer:   "",
 		}
-		time.Sleep(200 * time.Millisecond) // give time to process tx and save in db
+		time.Sleep(400 * time.Millisecond) // give time to process tx and save in db
 
 		// then
 		select {
-		case r := <-tx1ResponseChannel:
-			require.Equal(t, tx1.String(), r.Hash.String())
-			require.Equal(t, metamorph_api.Status_ACCEPTED_BY_NETWORK, r.Status)
+		case r1 := <-tx1ResponseChannel:
+			require.Equal(t, tx1.String(), r1.Hash.String())
+			require.Equal(t, metamorph_api.Status_ACCEPTED_BY_NETWORK, r1.Status)
+		case r2 := <-tx2ResponseChannel:
+			require.Equal(t, tx2.String(), r2.Hash.String())
+			require.Equal(t, metamorph_api.Status_REJECTED, r2.Status)
 		default:
 			t.Fatal("did not receive update on the status channel")
 		}
 
-		tx1Stored, err := mtmStore.Get(context.Background(), tx1[:])
+		cacheTxs, err = cacheStore.MapGetAll(metamorph.CacheRegisteredTxsHash)
 		require.NoError(t, err)
-		require.NotNil(t, tx1Stored)
-		require.Equal(t, metamorph_api.Status_ACCEPTED_BY_NETWORK, tx1Stored.Status)
-
-		tx2Stored, err := mtmStore.Get(context.Background(), tx2[:])
-		require.ErrorIs(t, err, store.ErrNotFound)
-		require.Nil(t, tx2Stored)
-
-		txCount, err = cacheStore.MapLen(metamorph.CacheRegisteredTxsHash)
-		require.NoError(t, err)
-		require.Equal(t, int64(1), txCount)
+		require.NotNil(t, cacheTxs[tx1.String()])
+		require.Nil(t, cacheTxs[tx2.String()])
+		require.Nil(t, cacheTxs[txNotRegistered.String()])
+		require.Equal(t, numberOfCacheTxs-1, len(cacheTxs))
 	})
 }

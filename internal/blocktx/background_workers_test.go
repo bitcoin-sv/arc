332e8d7e2158885ab82ff64881676d0d438bd1e6
Michael Böckli <michael.boeckli@gmail.com>
2025-01-06 16:58:43 +0100

    refactor(ARCO-312): Fix test

--- internal/blocktx/background_workers_test.go
+++ internal/blocktx/background_workers_test.go
@@ -82,10 +82,6 @@ func TestStartFillGaps(t *testing.T) {
 
 					return tc.blockGaps, nil
 				},
-
-				GetBlocksWithMissingMerkleTreeIndexFunc: func(_ context.Context) ([]*store.Block, error) {
-					return nil, nil
-				},
 			}
 
 			peerMock := &mocks.PeerMock{

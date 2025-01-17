c6c0d959bc3381dca36e924070b6b8125fc750a2
Michael Böckli <michael.boeckli@gmail.com>
2025-01-17 16:10:20 +0100

    feat(ARCO-244): gofmt

--- config/config.go
+++ config/config.go
@@ -84,18 +84,18 @@ type PeerPortConfig struct {
 }
 
 type MetamorphConfig struct {
-	ListenAddr                              string        `mapstructure:"listenAddr"`
-	DialAddr                                string        `mapstructure:"dialAddr"`
-	Db                                      *DbConfig     `mapstructure:"db"`
-	ProcessorCacheExpiryTime                time.Duration `mapstructure:"processorCacheExpiryTime"`
-	UnseenTransactionRebroadcastingInterval time.Duration `mapstructure:"unseenTransactionRebroadcastingInterval"`
-	MaxRetries                              int           `mapstructure:"maxRetries"`
-	ProcessStatusUpdateInterval             time.Duration `mapstructure:"processStatusUpdateInterval"`
-	RecheckSeen                             RecheckSeen   `mapstructure:"recheckSeen"`
-	MonitorPeers                            bool          `mapstructure:"monitorPeers"`
-	Health                                  *HealthConfig `mapstructure:"health"`
-	RejectCallbackContaining                []string      `mapstructure:"rejectCallbackContaining"`
-	Stats                                   *StatsConfig  `mapstructure:"stats"`
+	ListenAddr                              string                               `mapstructure:"listenAddr"`
+	DialAddr                                string                               `mapstructure:"dialAddr"`
+	Db                                      *DbConfig                            `mapstructure:"db"`
+	ProcessorCacheExpiryTime                time.Duration                        `mapstructure:"processorCacheExpiryTime"`
+	UnseenTransactionRebroadcastingInterval time.Duration                        `mapstructure:"unseenTransactionRebroadcastingInterval"`
+	MaxRetries                              int                                  `mapstructure:"maxRetries"`
+	ProcessStatusUpdateInterval             time.Duration                        `mapstructure:"processStatusUpdateInterval"`
+	RecheckSeen                             RecheckSeen                          `mapstructure:"recheckSeen"`
+	MonitorPeers                            bool                                 `mapstructure:"monitorPeers"`
+	Health                                  *HealthConfig                        `mapstructure:"health"`
+	RejectCallbackContaining                []string                             `mapstructure:"rejectCallbackContaining"`
+	Stats                                   *StatsConfig                         `mapstructure:"stats"`
 	BlockchainNetwork                       *BlockchainNetwork[*MetamorphGroups] `mapstructure:"bcnet"`
 }
 

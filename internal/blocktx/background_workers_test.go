78656e4b4901aa04096e844ecb61027d0ac974a2
Michael Böckli <michael.boeckli@gmail.com>
2025-01-17 15:44:54 +0100

    feat(ARCO-285): Make lint

--- internal/blocktx/background_workers_test.go
+++ internal/blocktx/background_workers_test.go
@@ -8,16 +8,14 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/bitcoin-sv/arc/internal/blocktx"
 	"github.com/bitcoin-sv/arc/internal/blocktx/bcnet/blocktx_p2p"
 	"github.com/bitcoin-sv/arc/internal/blocktx/store"
 	storeMocks "github.com/bitcoin-sv/arc/internal/blocktx/store/mocks"
-
 	"github.com/bitcoin-sv/arc/internal/p2p"
 	p2pMocks "github.com/bitcoin-sv/arc/internal/p2p/mocks"
-	"github.com/bitcoin-sv/arc/internal/testdata"
-	"github.com/stretchr/testify/require"
-
 	"github.com/bitcoin-sv/arc/internal/testdata"
 )
 

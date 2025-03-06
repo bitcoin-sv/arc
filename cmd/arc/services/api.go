c56b6e252c7fc6fb1cacfc46d681591753709964
shota.silagadze <shota.silagadze@taal.com>
2025-03-06 13:09:11 +0400

    fix metamorph ping

--- cmd/arc/services/api.go
+++ cmd/arc/services/api.go
@@ -133,30 +133,30 @@ func StartAPIServer(logger *slog.Logger, arcConfig *config.ArcConfig) (func(), e
 	}
 
 	apiOpts = append(apiOpts, apiHandler.WithReadinessCheck(func() error {
-		// mtmClient := grpc_health_v1.NewHealthClient(conn)
-		// ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-		// defer cancel()
-
-		// resp, err := mtmClient.Check(ctx, &grpc_health_v1.HealthCheckRequest{Service: ""})
-		// if err != nil {
-		// 	return fmt.Errorf("health check failed for metamorph grpc: %v", err)
-		// }
-		// if resp.Status != grpc_health_v1.HealthCheckResponse_SERVING {
-		// 	return fmt.Errorf("metamorph grpc server not serving: %v", resp.Status)
-		// }
-
-		btxClient := grpc_health_v1.NewHealthClient(btcConn)
+		mtmClient := grpc_health_v1.NewHealthClient(conn)
 		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 		defer cancel()
 
-		resp, err := btxClient.Check(ctx, &grpc_health_v1.HealthCheckRequest{Service: ""})
+		resp, err := mtmClient.Check(ctx, &grpc_health_v1.HealthCheckRequest{Service: ""})
 		if err != nil {
-			return fmt.Errorf("health check failed for blocktx grpc: %v", err)
+			return fmt.Errorf("health check failed for metamorph grpc: %v", err)
+		}
+		if resp.Status != grpc_health_v1.HealthCheckResponse_SERVING {
+			return fmt.Errorf("metamorph grpc server not serving: %v", resp.Status)
 		}
 
-		if resp.Status != grpc_health_v1.HealthCheckResponse_SERVING {
-			return fmt.Errorf("blocktx grpc server not serving: %v", resp.Status)
-		}
+		// btxClient := grpc_health_v1.NewHealthClient(btcConn)
+		// ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		// defer cancel()
+
+		// resp, err := btxClient.Check(ctx, &grpc_health_v1.HealthCheckRequest{Service: ""})
+		// if err != nil {
+		// 	return fmt.Errorf("health check failed for blocktx grpc: %v", err)
+		// }
+
+		// if resp.Status != grpc_health_v1.HealthCheckResponse_SERVING {
+		// 	return fmt.Errorf("blocktx grpc server not serving: %v", resp.Status)
+		// }
 		return nil
 	}))
 

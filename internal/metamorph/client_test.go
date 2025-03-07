34b113f4fd5303c53f732f2a70618c99feb7a933
Michael Böckli <michael.boeckli@gmail.com>
2025-03-07 13:10:42 +0100

    refactor: use nats stream for submit-tx topic

--- internal/metamorph/client_test.go
+++ internal/metamorph/client_test.go
@@ -167,7 +167,7 @@ func TestClient_SubmitTransaction(t *testing.T) {
 			}
 			if tc.withMqClient {
 				mqClient := &mqMocks.MessageQueueClientMock{
-					PublishMarshalAsyncFunc: func(_ string, _ protoreflect.ProtoMessage) error { return tc.publishSubmitTxErr },
+					PublishMarshalFunc: func(_ context.Context, _ string, _ protoreflect.ProtoMessage) error { return tc.publishSubmitTxErr },
 				}
 				opts = append(opts, metamorph.WithMqClient(mqClient))
 			}
@@ -423,7 +423,7 @@ func TestClient_SubmitTransactions(t *testing.T) {
 			}
 			if tc.withMqClient {
 				mqClient := &mqMocks.MessageQueueClientMock{
-					PublishMarshalAsyncFunc: func(_ string, _ protoreflect.ProtoMessage) error {
+					PublishMarshalFunc: func(_ context.Context, _ string, _ protoreflect.ProtoMessage) error {
 						return tc.publishSubmitTxErr
 					},
 				}

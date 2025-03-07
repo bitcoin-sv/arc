7038ffda86a73363b34a35519e503bf050a619e2
Michael Böckli <michael.boeckli@gmail.com>
2025-03-07 10:46:16 +0100

    refactor: integration tests

--- pkg/message_queue/nats/client/nats_jetstream/integration_test/client_integration_test.go
+++ pkg/message_queue/nats/client/nats_jetstream/integration_test/client_integration_test.go
@@ -16,7 +16,6 @@ import (
 	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/proto"
 
-	"github.com/bitcoin-sv/arc/internal/mq"
 	"github.com/bitcoin-sv/arc/pkg/message_queue/nats/client/nats_jetstream"
 	"github.com/bitcoin-sv/arc/pkg/message_queue/nats/client/test_api"
 	"github.com/bitcoin-sv/arc/pkg/message_queue/nats/nats_connection"
@@ -29,6 +28,8 @@ var (
 	mqClient         *nats_jetstream.Client
 	logger           *slog.Logger
 	err              error
+	natsURL          string
+	containerID      string
 )
 
 func TestMain(m *testing.M) {
@@ -57,6 +58,8 @@ func testmain(m *testing.M) int {
 		log.Print(err)
 		return 1
 	}
+
+	containerID = resource.Container.ID
 
 	logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 
@@ -90,11 +93,25 @@ func TestPublish(t *testing.T) {
 		t.Skip("skipping integration test")
 	}
 
+	streamOppositeFunc := func(cl *nats_jetstream.Client, topic string, messageChan chan *test_api.TestMessage) {
+		err = cl.Subscribe(topic, func(bytes []byte) error {
+			serialized := &test_api.TestMessage{}
+			err := proto.Unmarshal(bytes, serialized)
+			if assert.NoError(t, err) {
+				messageChan <- serialized
+			}
+
+			return nil
+		})
+		require.NoError(t, err)
+	}
+
 	tt := []struct {
-		name     string
-		topic    string
-		opts     []nats_jetstream.Option
-		testFunc func(cl mq.MessageQueueClient, topic string, msg *test_api.TestMessage)
+		name         string
+		topic        string
+		opts         []nats_jetstream.Option
+		testFunc     func(cl *nats_jetstream.Client, topic string, msg *test_api.TestMessage)
+		oppositeFunc func(cl *nats_jetstream.Client, topic string, messageChan chan *test_api.TestMessage)
 	}{
 		{
 			name:  "publish marshal - work queue policy",
@@ -103,10 +120,11 @@ func TestPublish(t *testing.T) {
 				nats_jetstream.WithStream("pub-topic-1", "pub-topic-1-stream", jetstream.WorkQueuePolicy, false),
 				nats_jetstream.WithConsumer("pub-topic-1", "pub-topic-1-stream", "pub-topic-1-cons", false, jetstream.AckExplicitPolicy),
 			},
-			testFunc: func(cl mq.MessageQueueClient, topic string, msg *test_api.TestMessage) {
+			testFunc: func(cl *nats_jetstream.Client, topic string, msg *test_api.TestMessage) {
 				err = cl.PublishMarshal(context.TODO(), topic, msg)
 				require.NoError(t, err)
 			},
+			oppositeFunc: streamOppositeFunc,
 		},
 		{
 			name:  "publish marshal async - work queue policy",
@@ -115,32 +133,43 @@ func TestPublish(t *testing.T) {
 				nats_jetstream.WithStream("pub-topic-2", "pub-topic-2-stream", jetstream.WorkQueuePolicy, true),
 				nats_jetstream.WithConsumer("pub-topic-2", "pub-topic-2-stream", "pub-topic-2-cons", false, jetstream.AckExplicitPolicy),
 			},
-			testFunc: func(cl mq.MessageQueueClient, topic string, msg *test_api.TestMessage) {
+			testFunc: func(cl *nats_jetstream.Client, topic string, msg *test_api.TestMessage) {
 				err = cl.PublishMarshalAsync(topic, msg)
 				require.NoError(t, err)
 			},
+			oppositeFunc: streamOppositeFunc,
 		},
 		{
-			name:  "publish marshal async - interest policy",
+			name:  "publish marshal - interest policy",
 			topic: "pub-topic-3",
 			opts: []nats_jetstream.Option{
 				nats_jetstream.WithStream("pub-topic-3", "pub-topic-3-stream", jetstream.InterestPolicy, false),
 				nats_jetstream.WithConsumer("pub-topic-3", "pub-topic-3-stream", "pub-topic-3-cons", false, jetstream.AckExplicitPolicy),
 			},
-			testFunc: func(cl mq.MessageQueueClient, topic string, msg *test_api.TestMessage) {
+			testFunc: func(cl *nats_jetstream.Client, topic string, msg *test_api.TestMessage) {
 				err = cl.PublishMarshal(context.TODO(), topic, msg)
 				require.NoError(t, err)
 			},
+			oppositeFunc: streamOppositeFunc,
 		},
 		{
-			name:  "publish marshal async - interest policy",
+			name:  "publish marshal core",
 			topic: "pub-topic-4",
-			opts: []nats_jetstream.Option{
-				nats_jetstream.WithStream("pub-topic-4", "pub-topic-4-stream", jetstream.InterestPolicy, false),
-				nats_jetstream.WithConsumer("pub-topic-4", "pub-topic-4-stream", "pub-topic-4-cons", false, jetstream.AckExplicitPolicy),
-			},
-			testFunc: func(cl mq.MessageQueueClient, topic string, msg *test_api.TestMessage) {
+			opts:  []nats_jetstream.Option{},
+			testFunc: func(cl *nats_jetstream.Client, topic string, msg *test_api.TestMessage) {
 				err = cl.PublishMarshalCore(topic, msg)
+				require.NoError(t, err)
+			},
+			oppositeFunc: func(cl *nats_jetstream.Client, topic string, messageChan chan *test_api.TestMessage) {
+				err = cl.QueueSubscribe(topic, func(bytes []byte) error {
+					serialized := &test_api.TestMessage{}
+					err := proto.Unmarshal(bytes, serialized)
+					if assert.NoError(t, err) {
+						messageChan <- serialized
+					}
+
+					return nil
+				})
 				require.NoError(t, err)
 			},
 		},
@@ -160,16 +189,7 @@ func TestPublish(t *testing.T) {
 
 			// when
 			t.Log("subscribe to topic")
-			err = mqClientOpposite.Subscribe(tc.topic, func(bytes []byte) error {
-				serialized := &test_api.TestMessage{}
-				err := proto.Unmarshal(bytes, serialized)
-				if assert.NoError(t, err) {
-					messageChan <- serialized
-				}
-
-				return nil
-			})
-			require.NoError(t, err)
+			tc.oppositeFunc(mqClientOpposite, tc.topic, messageChan)
 
 			t.Log("publish")
 			for range 4 {
@@ -204,7 +224,7 @@ func TestSubscribe(t *testing.T) {
 		name     string
 		topic    string
 		opts     []nats_jetstream.Option
-		testFunc func(cl mq.MessageQueueClient, topic string, messageChan chan *test_api.TestMessage)
+		testFunc func(cl *nats_jetstream.Client, topic string, messageChan chan *test_api.TestMessage)
 	}{
 		{
 			name:  "subscribe - work queue policy",
@@ -213,7 +233,7 @@ func TestSubscribe(t *testing.T) {
 				nats_jetstream.WithStream("sub-topic-1", "sub-topic-1-stream", jetstream.WorkQueuePolicy, false),
 				nats_jetstream.WithConsumer("sub-topic-1", "sub-topic-1-stream", "sub-topic-1-cons", true, jetstream.AckExplicitPolicy),
 			},
-			testFunc: func(cl mq.MessageQueueClient, topic string, messageChan chan *test_api.TestMessage) {
+			testFunc: func(cl *nats_jetstream.Client, topic string, messageChan chan *test_api.TestMessage) {
 				err = cl.Subscribe(topic, func(bytes []byte) error {
 					serialized := &test_api.TestMessage{}
 					unmarshalErr := proto.Unmarshal(bytes, serialized)
@@ -233,7 +253,7 @@ func TestSubscribe(t *testing.T) {
 				nats_jetstream.WithStream("sub-topic-2", "sub-topic-2-stream", jetstream.InterestPolicy, false),
 				nats_jetstream.WithConsumer("sub-topic-2", "sub-topic-2-stream", "sub-topic-2-cons", true, jetstream.AckExplicitPolicy),
 			},
-			testFunc: func(cl mq.MessageQueueClient, topic string, messageChan chan *test_api.TestMessage) {
+			testFunc: func(cl *nats_jetstream.Client, topic string, messageChan chan *test_api.TestMessage) {
 				err = cl.SubscribeMsg(topic, func(msg jetstream.Msg) error {
 					serialized := &test_api.TestMessage{}
 					unmarshalErr := proto.Unmarshal(msg.Data(), serialized)
@@ -257,7 +277,7 @@ func TestSubscribe(t *testing.T) {
 			messageChan := make(chan *test_api.TestMessage, 10)
 
 			// subscribe with initialized consumer
-			mqClientOpposite, err := nats_jetstream.New(natsConnClient, logger, tc.opts...)
+			mqClientOpposite, err := nats_jetstream.New(natsConnOpposite, logger, tc.opts...)
 			require.NoError(t, err)
 
 			tc.testFunc(mqClient, tc.topic, messageChan)
@@ -279,6 +299,7 @@ func TestSubscribe(t *testing.T) {
 				case <-time.NewTimer(500 * time.Millisecond).C:
 					t.Fatal("timeout waiting for submitted txs")
 				case minedTxBlock := <-messageChan:
+					t.Log("received message")
 					require.Equal(t, minedTxBlock.Ok, tm.Ok)
 					counter++
 					if counter >= 4 {

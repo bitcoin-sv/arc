209a943346d0675899d13fb70977e1c870824ee0
Michael Böckli <michael.boeckli@gmail.com>
2025-04-22 11:41:06 +0200

    feat(BPAAS-2078): rpc client test

---
+++ internal/node_client/rpc_client_test.go
@@ -0,0 +1,37 @@
+package node_client_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/bitcoin-sv/arc/internal/node_client"
+)
+
+func TestRPCClient(t *testing.T) {
+	t.Helper()
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+
+	ctx := context.Background()
+
+	setup()
+	sut, err := node_client.NewRPCClient(host, hostPort, user, password)
+	require.NoError(t, err)
+
+	address, _ := node_client.FundNewWallet(t, bitcoind)
+
+	utxos := node_client.GetUtxos(t, bitcoind, address)
+	require.True(t, len(utxos) > 0, "No UTXOs availa`ble for the address")
+
+	t.Run("invalidate block", func(t *testing.T) {
+		// given
+		blockHash, err := bitcoind.Generate(1)
+		require.NoError(t, err)
+
+		err = sut.InvalidateBlock(ctx, blockHash[0])
+		require.NoError(t, err)
+	})
+}

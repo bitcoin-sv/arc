fa9aac8899159ca82dd05887d83767667eae9f6c
Domingo <>
2025-05-07 11:06:38 +0200

    BPAAS-2079: Adding the AutoHeal job and the tests

--- internal/blocktx/store/postgresql/auto_heal_orphans.go
+++ internal/blocktx/store/postgresql/auto_heal_orphans.go
@@ -6,58 +6,60 @@ import (
 	"github.com/bitcoin-sv/arc/internal/blocktx/blocktx_api"
 )
 
-// GetOrphansBackToNonOrphanAncestor recursively searches for blocks marked
-// as ORPHANED from the given hash - back to the first ORPHANED block. Then, it
-// tries to get the first non-orphaned ancestor of that orphan chain.
-//
-// It searches for the block whose hash matches the prevhash of the given block,
+// AutoHealOrphans searches for the LONGEST blocks in the last 1000
+// then recursively searches for blocks marked as ORPHANED
+// whose hash matches the prevhash of the given block,
 // and then repeats that recursively for each newly found orphaned block until
 // it has the entire orphaned chain.
 func (p *PostgreSQL) AutoHealOrphans(ctx context.Context) (healedOrphans []*blocktx_api.Block, err error) {
 	// The way this query works, is that the result from the first SELECT
-	// will be stored in the `orphans` variable, which is later used
+	// will be stored in the `recent_orphans` variable, which is later used
 	// for recursion in the second SELECT.
 	//
 	// Then entire recursion happens in the second SELECT, after UNION ALL,
-	// and the first SELECT is just to set up the `orphans` variable with
-	// the first, initial value. Then, the `orphans` variable is recursively
+	// and the first SELECT is just to set up the `recent_orphans` variable with
+	// the first, initial value. Then, the `recent_orphans` variable is recursively
 	// updated with values returned from the second SELECT.
 	q := `
-		WITH RECURSIVE orphans AS (
-			SELECT
-				hash
+		WITH RECURSIVE recent_orphans AS (
+			SELECT 	* FROM (SELECT 	hash
+									,prevhash
+									,merkleroot
+									,height
+									,processed_at
+									,status
+									,chainwork
+			                FROM blocktx.blocks
+							ORDER BY height DESC
+							LIMIT 1000
+			) recent_longest
+			WHERE recent_longest.status = $1
+			UNION ALL
+			SELECT child.hash
+				,child.prevhash
+				,child.merkleroot
+				,child.height
+				,child.processed_at
+				,child.status
+				,child.chainwork 
+			FROM recent_orphans as parent
+		   JOIN blocktx.blocks as child ON child.prevhash = parent.hash
+			WHERE child.processed_at IS NOT NULL
+			AND child.status = $2
+		)
+		SELECT 	hash
 				,prevhash
 				,merkleroot
 				,height
 				,processed_at
 				,status
 				,chainwork
-			FROM blocktx.blocks WHERE hash = $1 AND status = $2
-			UNION ALL
-			SELECT
-				b.hash
-				,b.prevhash
-				,b.merkleroot
-				,b.height
-				,b.processed_at
-				,b.status
-				,b.chainwork
-			FROM blocktx.blocks b JOIN orphans o ON o.prevhash = b.hash AND b.status = $2
-			WHERE b.processed_at IS NOT NULL
-		)
-		SELECT
-			hash
-		 ,prevhash
-		 ,merkleroot
-		 ,height
-		 ,processed_at
-		 ,status
-		 ,chainwork
-		FROM orphans
+		FROM recent_orphans
+		WHERE status = $2
 		ORDER BY height
 	`
 
-	rows, err := p.db.QueryContext(ctx, q, nil, blocktx_api.Status_ORPHANED)
+	rows, err := p.db.QueryContext(ctx, q, blocktx_api.Status_LONGEST, blocktx_api.Status_ORPHANED)
 	if err != nil {
 		return
 	}

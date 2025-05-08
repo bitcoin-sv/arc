874c289ee960fd067ea3ec7cf42e45e28eea44d4
shota silagadze <shota@MacBook-Pro-3.local>
2025-05-08 20:33:41 +0400

    feat(BPAAS-2099): fix conflicting tx statuses

--- internal/metamorph/store/postgresql/postgres_test.go
+++ internal/metamorph/store/postgresql/postgres_test.go
@@ -456,19 +456,19 @@ func TestPostgresDB(t *testing.T) {
 			{
 				Hash:         *testutils.RevChainhash(t, "3e0b5b218c344110f09bf485bc58de4ea5378e55744185edf9c1dafa40068ecd"), // update not expected - status is mined
 				Status:       metamorph_api.Status_DOUBLE_SPEND_ATTEMPTED,
-				CompetingTxs: []string{"3ce1e0c6cbbbe2118c3f80d2e6899d2d487f319ef0923feb61f3d26335b2225c"},
+				CompetingTxs: []string{"7809b730cbe7bb723f299a4e481fb5165f31175876392a54cde85569a18cc75f"},
 				Timestamp:    timestamp,
 			},
 			{
 				Hash:         *testutils.RevChainhash(t, "7809b730cbe7bb723f299a4e481fb5165f31175876392a54cde85569a18cc75f"), // update expected - old status < new status
-				Status:       metamorph_api.Status_REJECTED,
+				Status:       metamorph_api.Status_SEEN_ON_NETWORK,
 				CompetingTxs: []string{"1234"},
 				Error:        errors.New("double spend attempted"),
 				Timestamp:    timestamp,
 			},
 			{
 				Hash:         *testutils.RevChainhash(t, "3ce1e0c6cbbbe2118c3f80d2e6899d2d487f319ef0923feb61f3d26335b2225c"), // update not expected - hash non-existent in db
-				Status:       metamorph_api.Status_SEEN_ON_NETWORK,
+				Status:       metamorph_api.Status_DOUBLE_SPEND_ATTEMPTED,
 				CompetingTxs: []string{"1234"},
 				Timestamp:    timestamp,
 			},

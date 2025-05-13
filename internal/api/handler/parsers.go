8233bc3d34e2adba9532a832330b622910726487
Domingo <>
2025-05-13 14:07:38 +0200

    BPAAS-2118: Reducing complexity of parsers.go and blocktx/processor.go

--- internal/api/handler/parsers.go
+++ internal/api/handler/parsers.go
@@ -16,7 +16,7 @@ import (
 var ErrEmptyBody = errors.New("no transaction found - empty request body")
 
 func parseTransactionFromRequest(request *http.Request) ([]byte, error) {
-	body, err := io.ReadAll(request.Body)
+	body, err := getBodyFromRequest(request)
 	if err != nil {
 		return nil, err
 	}
@@ -46,7 +46,7 @@ func parseTransactionFromRequest(request *http.Request) ([]byte, error) {
 			return nil, err
 		}
 	case echo.MIMEOctetStream:
-		txHex = body
+		return body, nil
 	default:
 		return nil, fmt.Errorf("given content-type %s does not match any of the allowed content-types", contentType)
 	}
@@ -60,43 +60,23 @@ func parseTransactionFromRequest(request *http.Request) ([]byte, error) {
 
 func parseTransactionsFromRequest(request *http.Request) ([]byte, error) {
 	var txHex []byte
+	var err error
 
 	contentType := request.Header.Get(echo.HeaderContentType)
 
 	switch contentType {
 	case echo.MIMETextPlain:
-		scanner := bufio.NewScanner(request.Body)
-
-		for scanner.Scan() {
-			partialHex, err := hex.DecodeString(scanner.Text())
-			if err != nil {
-				return nil, err
-			}
-			txHex = append(txHex, partialHex...)
-		}
-
-		if err := scanner.Err(); err != nil {
+		if txHex, err = getTxHexFromMIMETextPlain(request); err != nil {
 			return nil, err
 		}
 	case echo.MIMEApplicationJSON:
-		var txBody api.POSTTransactionsJSONRequestBody
-		if err := json.NewDecoder(request.Body).Decode(&txBody); err != nil {
+		if txHex, err = getTxHexFromMIMEApplicationJSON(request); err != nil {
 			return nil, err
-		}
-
-		for _, tx := range txBody {
-			partialHex, err := hex.DecodeString(tx.RawTx)
-			if err != nil {
-				return nil, err
-			}
-			txHex = append(txHex, partialHex...)
 		}
 	case echo.MIMEOctetStream:
-		body, err := io.ReadAll(request.Body)
-		if err != nil {
+		if txHex, err = getTxHexFromMIMEOctetStream(request); err != nil {
 			return nil, err
 		}
-		txHex = body
 	default:
 		return nil, fmt.Errorf("given content-type %s does not match any of the allowed content-types", contentType)
 	}
@@ -106,4 +86,54 @@ func parseTransactionsFromRequest(request *http.Request) ([]byte, error) {
 	}
 
 	return txHex, nil
+}
+
+func getTxHexFromMIMEApplicationJSON(request *http.Request) ([]byte, error) {
+	var txHex []byte
+	var txBody api.POSTTransactionsJSONRequestBody
+	if err := json.NewDecoder(request.Body).Decode(&txBody); err != nil {
+		return nil, err
+	}
+	for _, tx := range txBody {
+		partialHex, err := hex.DecodeString(tx.RawTx)
+		if err != nil {
+			return nil, err
+		}
+		txHex = append(txHex, partialHex...)
+	}
+	return txHex, nil
+}
+
+func getTxHexFromMIMETextPlain(request *http.Request) ([]byte, error) {
+	var txHex []byte
+	scanner := bufio.NewScanner(request.Body)
+
+	for scanner.Scan() {
+		partialHex, err := hex.DecodeString(scanner.Text())
+		if err != nil {
+			return nil, err
+		}
+		txHex = append(txHex, partialHex...)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return txHex, nil
+}
+
+func getTxHexFromMIMEOctetStream(request *http.Request) ([]byte, error) {
+	return getBodyFromRequest(request)
+}
+
+func getBodyFromRequest(request *http.Request) ([]byte, error) {
+	body, err := io.ReadAll(request.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(body) == 0 {
+		return nil, ErrEmptyBody
+	}
+	return body, nil
 }
--- internal/api/handler/parsers_test.go
+++ internal/api/handler/parsers_test.go
@@ -17,7 +17,8 @@ func TestParseTransactionFromRequest(t *testing.T) {
 		tx      string
 		txBytes []byte
 
-		expectedHex []byte
+		expectedHex        []byte
+		invalidContentType bool
 	}{
 		{
 			name:        "valid Raw Transaction",
@@ -33,31 +34,43 @@ func TestParseTransactionFromRequest(t *testing.T) {
 			name: "invalid transaction",
 			tx:   "invalidTx",
 		},
+		{
+			name:               "invalid content type",
+			tx:                 "invalidTx",
+			invalidContentType: true,
+		},
 	}
 
 	for _, tc := range testCases {
 		var requests []*http.Request
 
-		for _, ct := range contentTypes {
+		if tc.invalidContentType {
 			var r *http.Request
-
-			switch ct {
-			case echo.MIMETextPlain:
-				r, _ = http.NewRequest("GET", "", bytes.NewBuffer([]byte(tc.tx)))
-				r.Header.Set(echo.HeaderContentType, ct)
-
-			case echo.MIMEApplicationJSON:
-				body := fmt.Sprintf("{\"rawTx\": \"%s\"}", tc.tx)
-				r, _ = http.NewRequest("GET", "", bytes.NewBuffer([]byte(body)))
-				r.Header.Set(echo.HeaderContentType, ct)
-
-			case echo.MIMEOctetStream:
-				body, _ := hex.DecodeString(tc.tx)
-				r, _ = http.NewRequest("GET", "", bytes.NewBuffer(body))
-				r.Header.Set(echo.HeaderContentType, ct)
-			}
-
+			body := fmt.Sprintf("{\"rawTx\": \"%s\"}", tc.tx)
+			r, _ = http.NewRequest("GET", "", bytes.NewBuffer([]byte(body)))
+			r.Header.Set(echo.HeaderContentType, echo.MIMEMultipartForm)
 			requests = append(requests, r)
+		} else {
+			for _, ct := range contentTypes {
+				var r *http.Request
+
+				switch ct {
+				case echo.MIMETextPlain:
+					r, _ = http.NewRequest("GET", "", bytes.NewBuffer([]byte(tc.tx)))
+					r.Header.Set(echo.HeaderContentType, ct)
+
+				case echo.MIMEApplicationJSON:
+					body := fmt.Sprintf("{\"rawTx\": \"%s\"}", tc.tx)
+					r, _ = http.NewRequest("GET", "", bytes.NewBuffer([]byte(body)))
+					r.Header.Set(echo.HeaderContentType, ct)
+				case echo.MIMEOctetStream:
+					body, _ := hex.DecodeString(tc.tx)
+					r, _ = http.NewRequest("GET", "", bytes.NewBuffer(body))
+					r.Header.Set(echo.HeaderContentType, ct)
+				}
+
+				requests = append(requests, r)
+			}
 		}
 
 		for _, req := range requests {

d4b9c5171b728a8ec93b0554e705c8202f4d9106
shota silagadze <shota@MacBook-Pro-3.local>
2025-05-20 12:41:01 +0400

    feat(BPAAS-2114): tx validation with go-bdk

--- internal/api/handler/default.go
+++ internal/api/handler/default.go
@@ -604,7 +604,7 @@ func (m ArcDefaultHandler) processTransactions(ctx context.Context, txsHex []byt
 			}
 
 			se := goscript.NewScriptEngine(m.network)
-			fmt.Println("shota", m.network, hex.EncodeToString(txsHex), utxo, height)
+			fmt.Println("shota", "test", hex.EncodeToString(txsHex), utxo, height)
 			err = se.VerifyScript(txsHex, utxo, height, true)
 			if err != nil {
 				return nil, nil, api.NewErrorFields(api.ErrStatusBadRequest, err.Error())

6a94ff91c9e95d927fcbd82015c10baa2adc1b07
shota silagadze <shota@MacBook-Pro-3.local>
2025-05-21 14:33:30 +0400

    feat(BPAAS-2114): tx validation with go-bdk

--- internal/validator/default/default_validator.go
+++ internal/validator/default/default_validator.go
@@ -52,6 +52,7 @@ func (v *DefaultValidator) ValidateTransaction(ctx context.Context, tx *sdkTx.Tr
 		tracing.EndTracing(span, spanErr)
 	}()
 
+	fmt.Println("shota aq", hex.EncodeToString(tx.Bytes()))
 	// 0) Check whether we have a complete transaction in extended format, with all input information
 	//    we cannot check the satoshi input, OP_RETURN is allowed 0 satoshis
 	if needsExtension(tx, feeValidation, scriptValidation) {
@@ -66,7 +67,7 @@ func (v *DefaultValidator) ValidateTransaction(ctx context.Context, tx *sdkTx.Tr
 	if err != nil {
 		return err
 	}
-
+	fmt.Println("shota aq 2", hex.EncodeToString(tx.Bytes()))
 	// 10) Reject if the sum of input values is less than sum of output values
 	// 11) Reject if transaction fee would be too low (minRelayTxFee) to get into an empty block.
 	switch feeValidation {
@@ -87,7 +88,7 @@ func (v *DefaultValidator) ValidateTransaction(ctx context.Context, tx *sdkTx.Tr
 	case validator.NoneFeeValidation:
 		// Do not handle the default case on purpose; we shouldn't assume that other types of validation should be omitted
 	}
-
+	fmt.Println("shota aq 3", hex.EncodeToString(tx.Bytes()))
 	// 12) The unlocking scripts for each input must validate against the corresponding output locking scripts
 	if scriptValidation == validator.StandardScriptValidation {
 		blockHeight, err := v.btxClient.CurrentBlockHeight(ctx)

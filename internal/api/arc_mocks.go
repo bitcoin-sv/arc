64a77a12a8011db4abeeb94d76282909ac514190
shota silagadze <shota@MacBook-Pro-3.local>
2025-05-26 16:35:38 +0400

    feat(BPAAS-2114): tx validation with go-bdk

--- internal/api/arc_mocks.go
+++ internal/api/arc_mocks.go
@@ -2,4 +2,4 @@ package api
 
 //go:generate moq -pkg mocks -out ./mocks/client_interface_mock.go ../../pkg/api ClientInterface
 
-//go:generate moq -pkg mocks -out ./mocks/script_verifier.go ../../internal/api ScriptVerifier
+//go:generate moq -pkg mocks -out ./mocks/script_verifier_mock.go ../../internal/api ScriptVerifier

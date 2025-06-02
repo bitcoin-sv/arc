798299b0c286b7d5967d6cc83a6c0fe3facf0373
shota silagadze <shota@MacBook-Pro-3.local>
2025-06-02 19:25:21 +0400

    feat(BPAAS-2127): fix stale tx status

--- test/submit_05_reorg_test.go
+++ test/submit_05_reorg_test.go
@@ -179,7 +179,11 @@ func TestReorg(t *testing.T) {
 	// verify that previous mined tx1 have updated block info
 	checkStatusBlockHash(t, tx1.TxID().String(), StatusMined, staleHash)
 
+	time.Sleep(5 * time.Second)
 	node_client.Generate(t, bitcoind, 1)
+	time.Sleep(5 * time.Second)
+	node_client.Generate(t, bitcoind, 1)
+	time.Sleep(5 * time.Second)
 
 	// verify that tx2 is now MINED_IN_STALE_BLOCK
 	checkStatusBlockHash(t, tx2.TxID().String(), StatusMinedInStaleBlock, tx2BlockHash)

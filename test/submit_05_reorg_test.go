d23464aae96da65f58cf6a1e7c3765986766b8a3
shota silagadze <shota@MacBook-Pro-3.local>
2025-06-03 15:38:34 +0400

    feat(BPAAS-2127): fix stale tx status

--- test/submit_05_reorg_test.go
+++ test/submit_05_reorg_test.go
@@ -185,7 +185,7 @@ func TestReorg(t *testing.T) {
 	checkStatusBlockHash(t, tx1.TxID().String(), StatusMined, staleHash)
 
 	// verify that tx2 is now MINED_IN_STALE_BLOCK
-	checkStatusBlockHash(t, tx2.TxID().String(), StatusMined, tx2BlockHash)
+	checkStatusBlockHash(t, tx2.TxID().String(), StatusMined, staleHash)
 
 	merklePathTx1 = getMerklePath(t, tx1.TxID().String())
 

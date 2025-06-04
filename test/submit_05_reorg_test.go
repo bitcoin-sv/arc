917fdb2aa97befd54aab9d8fb145796f6df7825c
shota silagadze <shota@MacBook-Pro-3.local>
2025-06-04 18:51:41 +0400

    feat(BPAAS-2127): fix stale tx status

--- test/submit_05_reorg_test.go
+++ test/submit_05_reorg_test.go
@@ -180,9 +180,6 @@ func TestReorg(t *testing.T) {
 	// verify that previous mined tx1 have updated block info
 	checkStatusBlockHash(t, tx1.TxID().String(), StatusMined, staleHash)
 
-	// verify that tx2 is now MINED_IN_STALE_BLOCK
-	checkStatusBlockHash(t, tx2.TxID().String(), StatusMinedInStaleBlock, tx2BlockHash)
-
 	merklePathTx1 = getMerklePath(t, tx1.TxID().String())
 
 	// expect 2 callbacks
@@ -190,10 +187,6 @@ func TestReorg(t *testing.T) {
 		select {
 		case status := <-callbackReceivedChan:
 			switch status.Txid {
-			// verify that callback for tx2 was received with status MINED_IN_STALE_BLOCK
-			case tx2.TxID().String():
-				require.Equal(t, StatusMinedInStaleBlock, status.TxStatus)
-				require.Equal(t, tx2BlockHash, *status.BlockHash)
 			// verify that callback for tx1 was received with status MINED and updated merkle path
 			case tx1.TxID().String():
 				require.Equal(t, StatusMined, status.TxStatus)

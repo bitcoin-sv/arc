37775e1be19dd1c6ffe082febd8d140d6b2f97f1
shota silagadze <shota@MacBook-Pro-3.local>
2025-07-08 02:10:46 +0400

    feat(BPAAS-2199): consider number of blocks when rejecting pending txs

--- internal/metamorph/processor_test.go
+++ internal/metamorph/processor_test.go
@@ -1378,8 +1378,13 @@ func TestRejectUnconfirmedRequested(t *testing.T) {
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
+			i := 0
 			blocktxClient := &btxMocks.ClientMock{
 				LatestBlocksFunc: func(_ context.Context, _ uint64) (*blocktx_api.LatestBlocksResponse, error) {
+					i++
+					if i == 1 {
+						return nil, errors.New("some error")
+					}
 					return tc.blocks, nil
 				},
 			}

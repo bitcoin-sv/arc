0960b1868d0fd9b61287919204ac7ce8fb9c7980
shota silagadze <shota@192.168.0.105>
2025-07-10 19:30:00 +0400

    feat(BPAAS-2220): add logs

--- internal/metamorph/processor_routines.go
+++ internal/metamorph/processor_routines.go
@@ -111,10 +111,12 @@ func RejectUnconfirmedRequested(ctx context.Context, p *Processor) []attribute.K
 		}
 
 		blocks := blocksSinceLastRequested.GetBlocks()
+		p.logger.Debug("Latest blocks since last requested", slog.Int("count", len(blocks)))
 		sinceLastProcessed := p.now().Sub(blocks[len(blocks)-1].ProcessedAt.AsTime())
 
 		// reject all txs which have been requested at least `rejectPendingSeenLastRequestedAgo` or the time since `rejectPendingBlocksSince` have been processed ago
 		requestedAgo := max(sinceLastProcessed, p.rejectPendingSeenLastRequestedAgo)
+		p.logger.Debug("Requested ago", slog.Duration("requestedAgo", requestedAgo), slog.Duration("sinceLastProcessed", sinceLastProcessed), slog.Duration("p.rejectPendingSeenLastRequestedAgo", p.rejectPendingSeenLastRequestedAgo))
 
 		txs, err = p.store.GetUnconfirmedRequested(ctx, requestedAgo, loadLimit, offset)
 		if err != nil {
@@ -124,6 +126,7 @@ func RejectUnconfirmedRequested(ctx context.Context, p *Processor) []attribute.K
 
 		offset += loadLimit
 
+		p.logger.Debug("Unconfirmed requested txs", slog.Int("count", len(txs)), slog.Duration("requestedAgo", requestedAgo), slog.Bool("enabled", p.rejectPendingSeenEnabled))
 		if p.rejectPendingSeenEnabled && len(txs) != 0 {
 			for _, tx := range txs {
 				p.logger.Info("Rejecting unconfirmed tx", slog.Bool("enabled", p.rejectPendingSeenEnabled), slog.String("hash", tx.String()))

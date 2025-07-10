faeb7608159367d129a62182586486a5d22c4db5
shotasilagadzetaal <139438093+shotasilagadzetaal@users.noreply.github.com>
2025-07-10 19:47:36 +0400

    feat(BPAAS-2220): add logs (#926)
    Co-authored-by: shota silagadze <shota@192.168.0.105>

--- internal/metamorph/processor_routines.go
+++ internal/metamorph/processor_routines.go
@@ -124,14 +124,17 @@ func RejectUnconfirmedRequested(ctx context.Context, p *Processor) []attribute.K
 
 		offset += loadLimit
 
-		if p.rejectPendingSeenEnabled && len(txs) != 0 {
+		p.logger.Debug("Unconfirmed requested txs", slog.Int("count", len(txs)), slog.Duration("requestedAgo", requestedAgo), slog.Bool("enabled", p.rejectPendingSeenEnabled))
+		if len(txs) != 0 {
 			for _, tx := range txs {
 				p.logger.Info("Rejecting unconfirmed tx", slog.Bool("enabled", p.rejectPendingSeenEnabled), slog.String("hash", tx.String()))
-				p.statusMessageCh <- &metamorph_p2p.TxStatusMessage{
-					Start:  time.Now(),
-					Hash:   tx,
-					Status: metamorph_api.Status_REJECTED,
-					Err:    ErrRejectUnconfirmed,
+				if p.rejectPendingSeenEnabled {
+					p.statusMessageCh <- &metamorph_p2p.TxStatusMessage{
+						Start:  time.Now(),
+						Hash:   tx,
+						Status: metamorph_api.Status_REJECTED,
+						Err:    ErrRejectUnconfirmed,
+					}
 				}
 			}
 			totalRejected += len(txs)

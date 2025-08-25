98c606407be09291b6fe03229a37b041f75c261e
Michael Böckli <michael.boeckli@gmail.com>
2025-08-25 15:25:11 +0200

    fix(BPAAS-2279): Callback constraint url, tx id, tx status

--- internal/callbacker/store/postgresql/migrations/000006_sent_at.up.sql
+++ internal/callbacker/store/postgresql/migrations/000006_sent_at.up.sql
@@ -1,4 +1,4 @@
 ALTER TABLE callbacker.callbacks ADD COLUMN sent_at TIMESTAMPTZ;
 
 CREATE INDEX ix_callbacks_sent_at ON callbacker.callbacks (sent_at);
-ALTER TABLE callbacker.callbacks ADD CONSTRAINT unique_tx_id_status UNIQUE (tx_id, tx_status);
+ALTER TABLE callbacker.callbacks ADD CONSTRAINT unique_url_tx_id_status UNIQUE (url, tx_id, tx_status);

84a69545d24af6d8304d507ee1b6c141a3dd39a1
Michael Böckli <michael.boeckli@gmail.com>
2025-08-27 08:54:43 +0200

    refactor(BPAAS-2279): Unit tests

---
+++ internal/callbacker/processor_routines_test.go
@@ -0,0 +1,54 @@
+package callbacker_test
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/bitcoin-sv/arc/internal/callbacker"
+	"github.com/bitcoin-sv/arc/internal/callbacker/mocks"
+)
+
+func TestStartCallbackStoreCleanup(t *testing.T) {
+	tt := []struct {
+		name                     string
+		deleteFailedOlderThanErr error
+
+		expectedClearCalls int
+	}{
+		{
+			name: "success",
+
+			expectedClearCalls: 1,
+		},
+		{
+			name:                     "error deleting failed older than",
+			deleteFailedOlderThanErr: errors.New("some error"),
+
+			expectedClearCalls: 1,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			cbStore := &mocks.ProcessorStoreMock{
+				ClearFunc: func(_ context.Context, _ time.Time) error {
+					return tc.deleteFailedOlderThanErr
+				},
+			}
+
+			logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+			processor, err := callbacker.NewProcessor(nil, cbStore, nil, logger)
+			require.NoError(t, err)
+			defer processor.GracefulStop()
+			callbacker.CallbackStoreCleanup(processor)
+
+			require.Equal(t, tc.expectedClearCalls, len(cbStore.ClearCalls()))
+		})
+	}
+}
--- internal/callbacker/processor_test.go
+++ internal/callbacker/processor_test.go
@@ -2,56 +2,75 @@ package callbacker_test
 
 import (
 	"context"
-	"errors"
 	"log/slog"
 	"os"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
 
 	"github.com/bitcoin-sv/arc/internal/callbacker"
+	"github.com/bitcoin-sv/arc/internal/callbacker/callbacker_api"
 	"github.com/bitcoin-sv/arc/internal/callbacker/mocks"
+	"github.com/bitcoin-sv/arc/internal/callbacker/store"
+	mqMocks "github.com/bitcoin-sv/arc/internal/mq/mocks"
 )
 
-func TestStartCallbackStoreCleanup(t *testing.T) {
+func TestProcessor_StartStoreCallbackRequests(t *testing.T) {
 	tt := []struct {
-		name                     string
-		deleteFailedOlderThanErr error
-
-		expectedIterations int
+		name string
 	}{
 		{
 			name: "success",
-
-			expectedIterations: 4,
-		},
-		{
-			name:                     "error deleting failed older than",
-			deleteFailedOlderThanErr: errors.New("some error"),
-
-			expectedIterations: 4,
 		},
 	}
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
 			cbStore := &mocks.ProcessorStoreMock{
-				ClearFunc: func(_ context.Context, _ time.Time) error {
-					return tc.deleteFailedOlderThanErr
+				InsertFunc: func(_ context.Context, data []*store.CallbackData) (int64, error) {
+					return 4, nil
+				},
+			}
+
+			msg := &callbacker_api.SendRequest{
+				CallbackRouting: &callbacker_api.CallbackRouting{
+					Url:        "abc.com",
+					Token:      "1234",
+					AllowBatch: false,
+				},
+				Txid:         "xyz",
+				Status:       callbacker_api.Status_MINED,
+				MerklePath:   "",
+				ExtraInfo:    "",
+				CompetingTxs: nil,
+				BlockHash:    "",
+				BlockHeight:  0,
+				Timestamp:    nil,
+			}
+			data, err := proto.Marshal(msg)
+			require.NoError(t, err)
+
+			mqClient := &mqMocks.MessageQueueClientMock{
+				QueueSubscribeFunc: func(topic string, msgFunc func([]byte) error) error {
+					err := msgFunc(data)
+					require.NoError(t, err)
+					return nil
 				},
 			}
 
 			logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
-			processor, err := callbacker.NewProcessor(nil, cbStore, nil, logger)
+			processor, err := callbacker.NewProcessor(nil, cbStore, mqClient, logger)
 			require.NoError(t, err)
 			defer processor.GracefulStop()
 
-			processor.StartCallbackStoreCleanup()
+			err = processor.Subscribe()
+			require.NoError(t, err)
 
-			time.Sleep(90 * time.Millisecond)
+			processor.StartStoreCallbackRequests()
 
-			require.Equal(t, tc.expectedIterations, len(cbStore.ClearCalls()))
+			time.Sleep(500 * time.Millisecond)
 		})
 	}
 }

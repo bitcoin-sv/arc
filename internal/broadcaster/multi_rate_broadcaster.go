c78035ca8399c3dc8fa5e81a669ee88e2a15a428
Michael Böckli <michael.boeckli@gmail.com>
2025-09-26 14:26:29 +0200

    fix(BPAAS-2151): Sleep 2s between each initialization so that WoC request context deadline won't exceed

--- internal/broadcaster/multi_rate_broadcaster.go
+++ internal/broadcaster/multi_rate_broadcaster.go
@@ -64,6 +64,11 @@ func (mrb *MultiKeyRateBroadcaster) Start() error {
 
 	for _, rb := range mrb.rbs {
 		initWG.Add(1)
+
+		// Add wait time between each initialization so that WoC request context deadline won't exceed
+		const sleepDuration = 2 * time.Second
+		time.Sleep(sleepDuration)
+
 		go func() {
 			defer initWG.Done()
 

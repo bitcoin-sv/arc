c9afb9dbea5e8cd76893b71510a68a421c2a252f
Michael Böckli <michael.boeckli@gmail.com>
2025-10-17 16:47:24 +0200

    fix(BPAAS-2402): Generate blocks initially as calculated

--- test/init_test.go
+++ test/init_test.go
@@ -49,35 +49,34 @@ func setupSut() {
 	// fund node
 	const minNumbeOfBlocks = 101
 
-	if info.Blocks < minNumbeOfBlocks {
-		// generate blocks in part to ensure blocktx is able to process all blocks
-		blockBatch := float64(20)
-		if os.Getenv("TEST_LOCAL_MCAST") != "" {
-			blockBatch = float64(4)
-		}
+	blocksToGenerate := minNumbeOfBlocks - info.Blocks
 
-		for {
-			_, err = bitcoind.Generate(blockBatch)
-			if err != nil {
-				log.Fatalln(err)
-			}
+	log.Printf("generate %d blocks", blocksToGenerate)
 
-			// give time to send all INV messages
-			time.Sleep(5 * time.Second)
-
-			info, err = bitcoind.GetInfo()
-			if err != nil {
-				log.Fatalln(err)
-			}
-
-			log.Printf("block height: %d", info.Blocks)
-
-			missingBlocks := minNumbeOfBlocks - info.Blocks
-			if missingBlocks < 0 {
-				break
-			}
-		}
-
-		time.Sleep(15 * time.Second) // wait for fillGaps to fill eventual gaps
+	// generate blocks in part to ensure blocktx is able to process all blocks
+	blockBatch := int32(20)
+	if os.Getenv("TEST_LOCAL_MCAST") != "" {
+		blockBatch = 4
 	}
+
+	for blocksToGenerate > 0 {
+		_, err = bitcoind.Generate(float64(blockBatch))
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// give time to send all INV messages
+		time.Sleep(5 * time.Second)
+
+		info, err = bitcoind.GetInfo()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		log.Printf("block height: %d", info.Blocks)
+
+		blocksToGenerate = blocksToGenerate - blockBatch
+	}
+
+	time.Sleep(15 * time.Second) // wait for fillGaps to fill eventual gaps
 }

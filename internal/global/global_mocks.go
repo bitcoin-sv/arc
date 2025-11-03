905fb213ceaca0133cb11f4351c724a22676b942
Michael Böckli <michael.boeckli@gmail.com>
2025-11-03 11:59:14 +0100

    refactor(BPAAS-2420): test coverage

--- internal/global/global_mocks.go
+++ internal/global/global_mocks.go
@@ -3,3 +3,9 @@ package global
 //go:generate moq -pkg mocks -out ./mocks/transaction_handler_mock.go . TransactionHandler
 
 //go:generate moq -pkg mocks -out ./mocks/blocktx_client_mock.go . BlocktxClient
+
+//go:generate moq -pkg mocks -out ./mocks/stoppable_mock.go . Stoppable
+
+//go:generate moq -pkg mocks -out ./mocks/stoppable_with_error_mock.go . StoppableWithError
+
+//go:generate moq -pkg mocks -out ./mocks/stoppable_with_context_mock.go . StoppableWithContext
---
+++ internal/global/types_test.go
@@ -0,0 +1,47 @@
+package global_test
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"testing"
+
+	"github.com/bitcoin-sv/arc/internal/global"
+	"github.com/bitcoin-sv/arc/internal/global/mocks"
+)
+
+func TestStoppablesShutdown(t *testing.T) {
+	t.Run("shutdown stoppable", func(t *testing.T) {
+		// given
+		stoppables := global.Stoppables{&mocks.StoppableMock{ShutdownFunc: func() {}}, &mocks.StoppableMock{ShutdownFunc: func() {}}}
+
+		// when
+		stoppables.Shutdown()
+	})
+}
+
+func TestStoppablesWithErrorShutdown(t *testing.T) {
+	t.Run("shutdown stoppable", func(t *testing.T) {
+		// given
+		stoppables := global.StoppablesWithError{
+			&mocks.StoppableWithErrorMock{ShutdownFunc: func() error { return nil }},
+			&mocks.StoppableWithErrorMock{ShutdownFunc: func() error { return errors.New("some error") }},
+		}
+
+		// when
+		stoppables.Shutdown(slog.Default())
+	})
+}
+
+func TestStoppablesWithContextShutdown(t *testing.T) {
+	t.Run("shutdown stoppable", func(t *testing.T) {
+		// given
+		stoppables := global.StoppablesWithContext{
+			&mocks.StoppableWithContextMock{ShutdownFunc: func(_ context.Context) error { return nil }},
+			&mocks.StoppableWithContextMock{ShutdownFunc: func(_ context.Context) error { return errors.New("some error") }},
+		}
+
+		// when
+		stoppables.Shutdown(context.TODO(), slog.Default())
+	})
+}
